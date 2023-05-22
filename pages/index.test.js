355739adb7839593cdc96abdfd3a4e2bbf6f98d3
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:29:00 +0100

    Updating unit test for index.js

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,87 +1,31 @@
-Example Test code:
-    
-    import React from 'react';
-    import { render, fireEvent } from '@testing-library/react';
-    import Index from './pages/index';
-    
-    describe('Index Component', () => {
-      it('should render the component correctly with the expected UI elements, props, and state', () => {
-        const { getByText } = render(<Index />);
-    
-        expect(getByText('Welcome to the Index page!')).toBeInTheDocument();
-      });
-    
-      it('should handle props and state changes correctly', () => {
-        const { getByText } = render(<Index title="Foo" />);
-    
-        expect(getByText('Foo')).toBeInTheDocument();
-      });
-    
-      it('should trigger the expected action when an event handler is called', () => {
-        const { getByTestId } = render(<Index />);
-        const button = getByTestId('submit-button');
-        fireEvent.click(button);
-    
-        expect(button).toBeDisabled();
-      });
-    
-      it('should handle asynchronous operations correctly', async () => {
-        const { getByTestId } = render(<Index />);
-        const button = getByTestId('fetch-button');
-        fireEvent.click(button);
-    
-        expect(getByTestId('loading-spinner')).toBeInTheDocument();
-      });
-    
-      it('should handle form validation logic correctly', () => {
-        const { getByTestId } = render(<Index />);
-        const input = getByTestId('name-input');
-        fireEvent.change(input, { target: { value: 'foo' } });
-    
-        expect(input).toHaveValue('foo');
-      });
-    
-      it('should render the correct components based on the URL or route changes', () => {
-        const { getByTestId } = render(<Index />);
-        const link = getByTestId('link');
-        fireEvent.click(link);
-    
-        expect(getByTestId('other-page')).toBeInTheDocument();
-      });
-    
-      it('should maintain the expected data flow when using Redux or a state management library', () => {
-        const { getByTestId } = render(<Index />);
-        const button = getByTestId('redux-button');
-        fireEvent.click(button);
-    
-        expect(getByTestId('state-value')).toHaveTextContent('foo');
-      });
-    
-      it('should handle component lifecycle methods correctly', () => {
-        const { getByTestId } = render(<Index />);
-        const button = getByTestId('lifecycle-button');
-        fireEvent.click(button);
-    
-        expect(getByTestId('lifecycle-value')).toHaveTextContent('foo');
-      });
-    
-      it('should match the saved snapshot', () => {
-        const { container } = render(<Index />);
-    
-        expect(container).toMatchSnapshot();
-      });
-    
-      it('should meet accessibility standards', () => {
-        const { container } = render(<Index />);
-    
-        expect(container).toHaveNoA11yViolations();
-      });
-    
-      it('should handle error conditions, edge cases, and unexpected inputs correctly', () => {
-        const { getByText } = render(<Index />);
-        const button = getByText('Error Button');
-        fireEvent.click(button);
-    
-        expect(getByText('Something went wrong!')).toBeInTheDocument();
-      });
-    });
+import React from 'react';
+
+const IndexPage = () => {
+  const [name, setName] = React.useState('');
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={handleChange} />
+      <p>Hello, {name}!</p>
+    </div>
+  );
+};
+
+export default IndexPage;
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import IndexPage from './index';
+
+test('it should display the name when entered in the input', () => {
+  const { getByText, getByPlaceholderText } = render(<IndexPage />);
+
+  const input = getByPlaceholderText('Name');
+  fireEvent.change(input, { target: { value: 'John' } });
+
+  expect(getByText('Hello, John!')).toBeInTheDocument();
+});
