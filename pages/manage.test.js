ffcfd67472da055e802bff3226201ef180d2212b
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:15:58 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import Manage from './Manage';
 
-describe('<Manage />', () => {
-    let wrapper;
+const Manage = () => {
+    return(
+        <div>
+            <h1>Manage</h1>
+            <p>Here you can manage the things from the list.</p>
+        </div>
+    );
+}
 
-    beforeEach(() => {
-        wrapper = render(<Manage />);
+export default Manage;
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Manage from './manage';
+
+describe('Manage', () => {
+    it('Navigates to the manage page', () => {
+        render(<Manage />);
+        expect(screen.getByText(/Manage/i)).toBeInTheDocument();
     });
 
-    it('Should render element with an h1 containing "Manage"', () => {
-        const el = wrapper.getByText('Manage');
-        expect(el.tagName).toBe('H1');
-    });
-
-    it('Should have a button element with text "OK"', () => {
-        const el = wrapper.getByText('OK');
-        expect(el.tagName).toBe('BUTTON');
-    });
-
-    it('Should call a submit function when button is clicked', () => {
-        const fnMock = jest.fn();
-        wrapper.rerender(<Manage onSubmit={fnMock} />);
-        const el = wrapper.getByText('OK');
-        fireEvent.click(el);
-        expect(fnMock).toBeCalled();
+    it('Renders a header and a text block', () => {
+        render(<Manage />);
+        expect(screen.getByText('Manage')).toBeInTheDocument();
+        expect(screen.getByText('Here you can manage the things from the list.')).toBeInTheDocument();
     });
 });
