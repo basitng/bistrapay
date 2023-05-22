eed5b39f06bc1cabf1378b7e3c7165e5063a1a88
Basit Ng <basitng2004@gmail.com>
2023-05-22 12:10:33 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
 import NavBar from './NavBar';
 
 describe('NavBar', () => {
-  it('renders the NavBar component correctly', () => {
-    render(<NavBar />);
-    expect(screen.queryByTestId('nav-bar')).toBeTruthy();
-  });
-
-  it('renders the NavBar component with correct title', () => {
-    render(<NavBar title="Home Page" />);
-    expect(screen.queryByText('Home Page')).toBeTruthy();
-  });
+    it('should render the navigation bar', () => {
+        render(<NavBar />);
+        expect(screen.getByRole('navigation')).toBeInTheDocument();
+    });
+    it('contains the list of navigation links', () => {
+        const { getByText } = render(<NavBar />);
+        expect(getByText('Home')).toBeInTheDocument();
+        expect(getByText('About')).toBeInTheDocument();
+        expect(getByText('Contact')).toBeInTheDocument();
+    });
+    it('each navigation link should take the user to the expected page', () => {
+        const { getByTestId } = render(<NavBar />);
+        const homeLink = getByTestId('home-link');
+        const aboutLink = getByTestId('about-link');
+        const contactLink = getByTestId('contact-link');
+        expect(homeLink.href).toContain('/home');
+        expect(aboutLink.href).toContain('/about');
+        expect(contactLink.href).toContain('/contact');
+    });
 });
