2e077eb23ede6f36641c35de10f3f7ab390cef58
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:44:22 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,43 +1,25 @@
 import React from 'react';
 
-const users = [
-    {name: "John", age: 30, email: "john@example.com"},
-    {name: "Mary", age: 25, email: "mary@example.com"}
-];
+const Users = () => {
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>This page displays all registered users on the app.</p>
+    </div>
+  );
+};
 
-const UserList = () => (
-    <>
-        <h1>Users</h1>
-        <ul>
-            {users.map(user => (
-                <li key={user.name}>
-                    <span>Name: {user.name} </span>
-                    <span>Age: {user.age} </span>
-                    <span>Email: {user.email} </span>
-                </li>
-            ))}
-        </ul>
-    </>
-);
-
-export default UserList;
+export default Users;
 
 // Unit Test
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import UserList from './users';
+import Users from './users';
 
-describe('UserList', () => {
-    test('should render the list of users', () => {
-        render(<UserList />);
-        const title = screen.getByText('Users');
-        const userNames = screen.getAllByText(/Name:/);
-        const userAges = screen.getAllByText(/Age:/);
-        const userEmails = screen.getAllByText(/Email:/);
-
-        expect(title).toBeInTheDocument();
-        expect(userNames).toHaveLength(2);
-        expect(userAges).toHaveLength(2);
-        expect(userEmails).toHaveLength(2);
-    });
+describe('Users', () => {
+  it('should render a Users page', () => {
+    render(<Users />);
+    expect(screen.getByText('Users')).toBeInTheDocument();
+    expect(screen.getByText('This page displays all registered users on the app.')).toBeInTheDocument();
+  });
 });
