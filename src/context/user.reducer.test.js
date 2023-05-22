4a880f0590c557a13103664f3766770230bdf7e3
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:39:20 +0100

    Updating unit test for user.reducer.js

--- src/context/user.reducer.test.js
+++ src/context/user.reducer.test.js
@@ -1,41 +1,35 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import {UserReducer} from "../reducers/user.reducer";
+import React from 'react';
+import { userReducer } from './user.reducer';
 
-describe('User Reducer', () => {
-  it('should return the initial state', () => {
-    expect(UserReducer(undefined, {})).toEqual({
-      user: null,
-      token: null
-    });
-  });
-  it('should handle SET_USER', () => {
-    const payload = {
-      user: { name: 'John' },
-      token: 'testToken'
+describe('userReducer', () => {
+  test('initial state', () => {
+    const expectedState = {
+      userName: '',
+      isLoggedIn: false
     };
-    expect(
-      UserReducer(undefined, {
-        type: 'SET_USER',
-        payload
-      })
-    ).toEqual({
-      user: { name: 'John' },
-      token: 'testToken'
-    });
+    expect(userReducer(undefined, {})).toEqual(expectedState);
   });
-  it('should handle LOGOUT', () => {
-    expect(
-      UserReducer(
-        {
-          user: { name: 'John' },
-          token: 'testToken'
-        }, {
-          type: 'LOGOUT'
-        })
-    ).toEqual({
-      user: null,
-      token: null
-    });
+
+  test('LOGIN', () => {
+    const action = {
+      type: 'LOGIN',
+      payload: { userName: 'John' }
+    };
+    const expectedState = {
+      userName: 'John',
+      isLoggedIn: true
+    };
+    expect(userReducer(undefined, action)).toEqual(expectedState);
+  });
+  
+  test('LOGOUT', () => {
+    const action = {
+      type: 'LOGOUT'
+    };
+    const expectedState = {
+      userName: '',
+      isLoggedIn: false
+    };
+    expect(userReducer(undefined, action)).toEqual(expectedState);
   });
 });
