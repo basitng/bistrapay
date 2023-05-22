16e21012cffc1d3fcfd8e96b28dc05b7ed934c9a
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:16:53 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,28 +1,13 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import NavBar from "./NavBar";
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import NavBar from './NavBar';
 
-describe("NavBar component", () => {
-  it("should render correctly", () => {
-    const { getByTestId } = render(<NavBar />);
-    expect(getByTestId("navbar")).toBeInTheDocument();
-  });
+describe('NavBar', () => {
+    test('it should open menu when clicked', () => {
+        const { getByTestId } = render(<NavBar />);
 
-  it("should show the correct number of navigation links", () => {
-    const { getAllByTestId } = render(<NavBar />);
-    expect(getAllByTestId("nav-link")).toHaveLength(3);
-  });
+        fireEvent.click(getByTestId("menu-button"))
 
-  it("should show the correct navigation link text", () => {
-    const { getByText } = render(<NavBar />);
-    expect(getByText("Home")).toBeInTheDocument();
-    expect(getByText("About")).toBeInTheDocument();
-    expect(getByText("Contact")).toBeInTheDocument();
-  });
-
-  it("should open the dropdown menu when the hamburger icon is clicked", () => {
-    const { getByTestId, getByRole } = render(<NavBar />);
-    fireEvent.click(getByTestId("hamburger-icon"));
-    expect(getByRole("listbox")).toBeInTheDocument();
-  });
+        expect(getByTestId("menu")).toBeVisible();
+    });
 });
