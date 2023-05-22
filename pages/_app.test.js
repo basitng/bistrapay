5b8df6a6d38549a1bc6ba96f960724bfdb23a226
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:36:07 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './_app';
 
-describe('App', () => {
-    it('should render without errors', () => {
-        const { getByText } = render(<App />);
-        const titleElement = getByText(/My App/i);
-        expect(titleElement).toBeInTheDocument();
-    });
+const App = () => {
+  const [state, setState] = React.useState("");
+  const handleChange = (e) => {
+    setState(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={state} onChange={handleChange} />
+    </div>
+  );
+};
+
+export default App;
+
+// Unit Test
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./_app";
+
+describe("App component", () => {
+  it("should render the input field", () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("input")).toBeInTheDocument();
+  });
+
+  it("should update the state on input change", () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId("input");
+    fireEvent.change(input, { target: { value: "Test value" } });
+    expect(input.value).toBe("Test value");
+  });
 });
