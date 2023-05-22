59bfea10c8688bb246eb7db924d008179f86b68b
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:40:04 +0100

    Updating unit test for users.context.js

--- src/context/users.context.test.js
+++ src/context/users.context.test.js
@@ -1,40 +1,63 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { UsersContext } from './users.context';
 
 describe('UsersContext', () => {
-  afterEach(cleanup);
-
-  it('should render the users context', () => {
-    const user = [
-      {
-        id: 1,
-        name: 'Bob',
-        email: 'bob@example.com',
-      },
-      {
-        id: 2,
-        name: 'John',
-        email: 'john@example.com',
-      },
-    ];
-
-    const { queryByTestId } = render(
-      <UsersContext.Provider value={user}>
-        <div data-testid="users-context">
-          {user &&
-            user.map(({ id, name, email }) => (
-              <div key={id} data-testid="user">
-                <p>Name: {name}</p>
-                <p>Email: {email}</p>
-              </div>
-            ))}
+  it('should render the context properly', () => {
+    const initialState = {
+      users: [],
+      isLoading: true
+    };
+    
+    const { getByTestId } = render(
+      <UsersContext.Provider value={initialState}>
+        <div data-testid="context-wrapper">
+          <h1>Users Context</h1>
         </div>
-      </UsersContext.Provider>,
+      </UsersContext.Provider>
     );
 
-    expect(queryByTestId('users-context')).toBeTruthy();
-    expect(queryByTestId('user')).toBeTruthy();
-    expect(queryByTestId('user').children.length).toEqual(2);
+    const contextWrapper = getByTestId('context-wrapper');
+    expect(contextWrapper).toBeInTheDocument();
+  });
+
+  it('should update state when an action is fired', () => {
+    const initialState = {
+      users: [],
+      isLoading: true
+    };
+    
+    const mockAction = {
+      type: 'set-users',
+      payload: [
+        { id: 1, name: 'John Doe' },
+        { id: 2, name: 'Jane Doe' }
+      ]
+    };
+    
+    const reducer = (state, action) => {
+      switch (action.type) {
+        case 'set-users':
+          return {
+            ...state,
+            users: action.payload,
+            isLoading: false
+          };
+        default:
+          return state;
+      }
+    }
+    
+    const { getByTestId } = render(
+      <UsersContext.Provider value={{...initialState, reducer}}>
+        <div data-testid="context-wrapper">
+          <h1>Users Context</h1>
+        </div>
+      </UsersContext.Provider>
+    );
+    
+    fireEvent.click(getByTestId('context-wrapper'));
+    expect(initialState.isLoading).toBe(false);
+    expect(initialState.users).toEqual(mockAction.payload);
   });
 });
