55b6fb28f5430814f6f65fbc9b366d50061d480b
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:19:48 +0100

    Updating unit test for user.reducer.js

--- src/context/user.reducer.test.js
+++ src/context/user.reducer.test.js
@@ -1,41 +1,117 @@
-import React from 'react';
-import { userReducer } from './user.reducer';
+import React from "react";
 
-describe('userReducer', () => {
-  it('should return the initial state', () => {
-    expect(userReducer(undefined, {})).toEqual({
-      loading: false,
-      user: undefined,
-    });
+import { reducer, userReducer, initialState } from "./user.reducer";
+import {
+  FETCH_USERS_REQUEST,
+  FETCH_USERS_SUCCESS,
+  FETCH_USERS_FAILURE,
+  UPDATE_USER,
+} from "./user.actions";
+
+describe("User Reducer", () => {
+  it("should return the initial state", () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
   });
 
-  it('should handle USER_REQUEST', () => {
+  it("should return the initial state when action type is not found", () => {
+    expect(reducer(initialState, { type: "SOME_ACTION" })).toEqual(initialState);
+  });
+  
+  it("should update state correctly when FETCH_USERS_REQUEST is dispatched", () => {
     expect(
-      userReducer({ loading: false, user: undefined }, { type: 'USER_REQUEST' })
-    ).toEqual({
-      loading: true,
-      user: undefined,
-    });
+      reducer(initialState, { type: FETCH_USERS_REQUEST })
+    ).toEqual({ loading: true, users: [] });
   });
 
-  it('should handle USER_SUCCESS', () => {
+  it("should update state correctly when FETCH_USERS_SUCCESS is dispatched", () => {
+    const users = [
+      { id: 1, name: "Betty Hall" },
+      { id: 2, name: "Cal Thomas" }
+    ];
     expect(
-      userReducer(
-        { loading: true, user: undefined },
-        { type: 'USER_SUCCESS', payload: { name: 'John Doe' } }
-      )
-    ).toEqual({
-      loading: false,
-      user: { name: 'John Doe' },
-    });
+      reducer(initialState, {
+        type: FETCH_USERS_SUCCESS,
+        payload: { data: users }
+      })
+    ).toEqual({ loading: false, users });
   });
 
-  it('should handle USER_FAILURE', () => {
+  it("should update state correctly when FETCH_USERS_FAILURE is dispatched", () => {
+    const error = "Failed to fetch users";
     expect(
-      userReducer({ loading: true, user: undefined }, { type: 'USER_FAILURE' })
-    ).toEqual({
-      loading: false,
-      user: undefined,
-    });
+      reducer(initialState, { type: FETCH_USERS_FAILURE, payload: { error } })
+    ).toEqual({ loading: false, users: [], error });
+  });
+
+  it("should update state correctly when UPDATE_USER is dispatched", () => {
+    const oldUsers = [
+      { id: 1, name: "Betty Hall" },
+      { id: 2, name: "Cal Thomas" }
+    ];
+    const newUserData = { id: 1, name: "Bob Hall" };
+    const expectedUsers = [
+      { id: 1, name: "Bob Hall" },
+      { id: 2, name: "Cal Thomas" }
+    ];
+    expect(
+      reducer({ loading: false, users: oldUsers }, {
+        type: UPDATE_USER,
+        payload: { data: newUserData, id: 1 }
+      })
+    ).toEqual({ loading: false, users: expectedUsers });
   });
 });
+
+// Unit Tests with React Testing Library
+
+import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import { reducer, userReducer, initialState } from './user.reducer';
+import { 
+    FETCH_USERS_REQUEST, 
+    FETCH_USERS_SUCCESS, 
+    FETCH_USERS_FAILURE, 
+    UPDATE_USER
+ } from './user.actions';
+ 
+ describe("User Reducer", () => {
+     afterEach(cleanup);
+     
+     it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual(initialState);
+     });
+     
+     it('should return the initial state when action type is not found', () => {
+        expect(reducer(initialState, {type: "SOME_ACTION"})).toEqual(initialState);
+     });
+     
+     it('should update state correctly when FETCH_USERS_REQUEST is dispatched', () => {
+        expect(reducer(initialState, { type: FETCH_USERS_REQUEST })).toEqual({ loading: true, users: [] });
+     });
+     
+     it('should update state correctly when FETCH_USERS_SUCCESS is dispatched', () => {
+        const users = [
+            {id: 1, name: 'Betty Hall'},
+            {id: 2, name: 'Cal Thomas'}
+        ];
+        expect(reducer(initialState, { type: FETCH_USERS_SUCCESS, payload: {data: users} })).toEqual({ loading: false, users }); 
+     });
+     
+     it('should update state correctly when FETCH_USERS_FAILURE is dispatched', () => {
+        const error = 'Failed to fetch users';
+        expect(reducer(initialState, { type: FETCH_USERS_FAILURE, payload: {error} })).toEqual({ loading: false, users: [], error }); 
+     });
+     
+     it('should update state correctly when UPDATE_USER is dispatched', () => {
+        const oldUsers = [
+            {id: 1, name: 'Betty Hall'},
+            {id: 2, name: 'Cal Thomas'}
+        ];
+        const newUserData = {id: 1, name: 'Bob Hall'};
+        const expectedUsers = [
+            {id: 1, name: 'Bob Hall'},
+            {id: 2, name: 'Cal Thomas'}
+        ];
+        expect(reducer({ loading: false, users: oldUsers }, {type: UPDATE_USER, payload: {data: newUserData, id: 1}})).toEqual({ loading: false, users: expectedUsers }); 
+     });
+ });
