ed51f3f87958ec889d17f3ed98fec02c01c00298
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:43:01 +0100

    Updating unit test for hello.js

--- pages/api/hello.test.js
+++ pages/api/hello.test.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 
-function Hello(props) {
-  return (
-    <div>
-      <h1>Hello, {props.name}!</h1>
-    </div>
-  );
-}
+const Hello = () => (
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a simple React component.</p>
+  </div>
+)
 
-describe('Hello component', () => {
-  it('should render a name correctly', () => {
-    const { getByText } = render(<Hello name="John" />);
-    const helloElement = getByText(/Hello, John!/i);
-    expect(helloElement).toBeInTheDocument();
-  });
+export default Hello;
+
+import { render, screen } from '@testing-library/react';
+
+describe('Hello', () => {
+    it('should render the component', () => {
+        render(<Hello />);
+        expect(screen.getByText('Hello World')).toBeInTheDocument();
+        expect(screen.getByText('This is a simple React component.')).toBeInTheDocument();
+    });
 });
