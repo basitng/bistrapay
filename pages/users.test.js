3df2ebc65383b8f98cb25e5524ae940f4609895f
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:11:16 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,25 +1,36 @@
-import React from 'react';
-
-const Users = () => {
-  return (
-    <div>
-      <h1>Users</h1>
-      <p>This page displays all registered users on the app.</p>
-    </div>
-  );
-};
-
-export default Users;
-
-// Unit Test
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Users from './users';
-
-describe('Users', () => {
-  it('should render a Users page', () => {
-    render(<Users />);
-    expect(screen.getByText('Users')).toBeInTheDocument();
-    expect(screen.getByText('This page displays all registered users on the app.')).toBeInTheDocument();
-  });
-});
+//import React from 'react';
+    import { render, fireEvent, screen } from '@testing-library/react';
+    import Users from './Users';
+    
+    describe('Users component', () => {
+        test('it should display number of users and a greeting message when rendered', () => {
+            // Arrange
+            const numUsers = 5;
+            
+            // Act
+            render(<Users numUsers={numUsers} />);
+            
+            // Assert
+            const userCount = screen.getByText('There are 5 users in the system');
+            const greetingMsg = screen.getByText(/Welcome to the users page/);
+            
+            expect(userCount).toBeInTheDocument();
+            expect(greetingMsg).toBeInTheDocument();
+        });
+        
+        test('it should update the user greeting message when user clicks the button', () => {
+            // Arrange
+            const numUsers = 5;
+            
+            // Act
+            render(<Users numUsers={numUsers} />);
+            const button = screen.getByRole('button');
+        
+            fireEvent.click(button);
+            
+            // Assert
+            const updatedGreetingMsg = screen.getByText(/Thank you for visiting/);
+            
+            expect(updatedGreetingMsg).toBeInTheDocument();
+        });
+    });
