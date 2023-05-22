2404927df2e8b03da44b0565271423033a431f0a
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:30:54 +0100

    Updating unit test for index.js

--- src/components/index.test.js
+++ src/components/index.test.js
@@ -1,70 +1,24 @@
 import React from 'react';
-    import {render, fireEvent, screen, cleanup} from '@testing-library/react';
-    import Index from './index';
-    
-    describe('Index component', () => {
-      afterEach(cleanup);
-    
-      it('should render the component', () => {
-        const {getByText} = render(<Index />);
-        expect(getByText('Index Component')).toBeInTheDocument();
-      });
-    
-      it('should have the expected props and state', () => {
-        const {getByTestId} = render(<Index message="Hello world!" />);
-        expect(getByTestId('message')).toHaveTextContent('Hello world!');
-      });
-    
-      it('should handle click events', () => {
-        const {getByTestId} = render(<Index />);
-        const button = getByTestId('button');
-        fireEvent.click(button);
-        expect(getByTestId('message')).toHaveTextContent('You clicked the button!');
-      });
-    
-      it('should handle change events', () => {
-        const {getByTestId} = render(<Index />);
-        const input = getByTestId('input');
-        fireEvent.change(input, {target: {value: 'Hello world!'}});
-        expect(getByTestId('message')).toHaveTextContent('Hello world!');
-      });
-    
-      it('should handle route changes', () => {
-        const {getByTestId} = render(<Index />);
-        const link = getByTestId('link');
-        fireEvent.click(link);
-        expect(screen.getByText('Route Changed!')).toBeInTheDocument();
-      });
-    
-      it('should handle redux actions', () => {
-        const {getByTestId} = render(<Index />);
-        const actionButton = getByTestId('action-button');
-        fireEvent.click(actionButton);
-        expect(getByTestId('message')).toHaveTextContent('Redux action triggered!');
-      });
-    
-      it('should handle form submissions', () => {
-        const {getByTestId} = render(<Index />);
-        const form = getByTestId('form');
-        fireEvent.submit(form);
-        expect(getByTestId('message')).toHaveTextContent('Form submitted!');
-      });
-    
-      it('should match the snapshot', () => {
-        const {container} = render(<Index />);
-        expect(container).toMatchSnapshot();
-      });
-    
-      it('should handle accessibility', () => {
-        const {getByText} = render(<Index />);
-        expect(getByText('Index Component')).toBeInTheDocument();
-        expect(getByText('Index Component')).toHaveAttribute('aria-label');
-      });
-    
-      it('should handle errors', () => {
-        const {getByTestId} = render(<Index />);
-        const errorButton = getByTestId('error-button');
-        fireEvent.click(errorButton);
-        expect(getByTestId('message')).toHaveTextContent('An error occurred!');
-      });
-    });
+import { render, cleanup } from '@testing-library/react';
+import Index from './index';
+
+describe('Index component', () => {
+  afterEach(cleanup);
+
+  it('should render without crashing', () => {
+    const { container } = render(<Index />);
+    expect(container).toBeTruthy();
+  });
+
+  it('should render a div', () => {
+    const { container } = render(<Index />);
+    const div = container.querySelector('div');
+    expect(div).toBeTruthy();
+  });
+
+  it('should render the correct text', () => {
+    const { getByText } = render(<Index />);
+    const text = getByText('This is the index component.');
+    expect(text).toBeTruthy();
+  });
+});
