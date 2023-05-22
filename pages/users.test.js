be14a90c60ba6892ed0e55f98f8e16c0249c4603
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:16:35 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,36 +1,11 @@
-//import React from 'react';
-    import { render, fireEvent, screen } from '@testing-library/react';
-    import Users from './Users';
-    
-    describe('Users component', () => {
-        test('it should display number of users and a greeting message when rendered', () => {
-            // Arrange
-            const numUsers = 5;
-            
-            // Act
-            render(<Users numUsers={numUsers} />);
-            
-            // Assert
-            const userCount = screen.getByText('There are 5 users in the system');
-            const greetingMsg = screen.getByText(/Welcome to the users page/);
-            
-            expect(userCount).toBeInTheDocument();
-            expect(greetingMsg).toBeInTheDocument();
-        });
-        
-        test('it should update the user greeting message when user clicks the button', () => {
-            // Arrange
-            const numUsers = 5;
-            
-            // Act
-            render(<Users numUsers={numUsers} />);
-            const button = screen.getByRole('button');
-        
-            fireEvent.click(button);
-            
-            // Assert
-            const updatedGreetingMsg = screen.getByText(/Thank you for visiting/);
-            
-            expect(updatedGreetingMsg).toBeInTheDocument();
-        });
-    });
+import React from 'react'; 
+import { render } from '@testing-library/react';
+import Users from './users';
+ 
+describe('Users component', () => {
+  it('renders name successfully', () => {
+    const { getByText } = render(<Users />);
+    const userNameNode = getByText('John Doe');
+    expect(userNameNode).toBeInTheDocument();
+  });
+});
