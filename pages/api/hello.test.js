36d086283da9fe5fd7a84d6b2072a3a0c60ca0b5
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:28:33 +0100

    Updating unit test for hello.js

--- pages/api/hello.test.js
+++ pages/api/hello.test.js
@@ -1,28 +1,12 @@
-import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
-import Hello from './pages/api/hello';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Hello from "../pages/api/hello";
 
-describe('Hello', () => {
-  test('renders with the expected UI elements', () => {
-    const { getByText, getByTestId } = render(<Hello />);
-    expect(getByText('Hello World!')).toBeInTheDocument();
-    expect(getByTestId('greeting-message')).toBeInTheDocument();
-  });
+describe("Hello page", () => {
+  test("renders hello world message", () => {
+    render(<Hello />);
+    const message = screen.getByText("Hello World!");
 
-  test('updates the greeting message when the input is changed', () => {
-    const { getByText, getByTestId, getByLabelText } = render(<Hello />);
-    expect(getByText('Hello World!')).toBeInTheDocument();
-    fireEvent.change(getByLabelText('Name:'), { target: { value: 'John' } });
-    expect(getByText('Hello John!')).toBeInTheDocument();
-    expect(getByTestId('greeting-message').value).toBe('Hello John!');
-  });
-
-  test('resets the greeting message when the reset button is clicked', () => {
-    const { getByText, getByTestId, getByLabelText, getByText } = render(<Hello />);
-    fireEvent.change(getByLabelText('Name:'), { target: { value: 'John' } });
-    expect(getByText('Hello John!')).toBeInTheDocument();
-    fireEvent.click(getByText('Reset'));
-    expect(getByText('Hello World!')).toBeInTheDocument();
-    expect(getByTestId('greeting-message').value).toBe('Hello World!');
+    expect(message).toBeInTheDocument();
   });
 });
