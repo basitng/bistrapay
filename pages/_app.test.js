7f750bc3fbe116998d26a67f8093e8cc628b2042
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:14:49 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
+import { render } from '@testing-library/react';
 
-describe('The _app component', () => {
-  it('should render the page', () => {
-    const {getByText, getByLabelText} = render(
-      <_app />
-    );
-    const titleElement = getByText(/my page/i);
-    expect(titleElement).toBeInTheDocument();
-    const inputElement = getByLabelText('input-text');
-    expect(inputElement).toBeInTheDocument();
-   });
-  
-  it('should change page title when text is entered', () => {
-    const {getByText, getByLabelText} = render(
-      <_app />
-    );
-    const titleElement = getByText(/my page/i);
-    expect(titleElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders without errors', () => {
+    const { queryByTestId } = render(<App />);
+    const component = queryByTestId('app-component'); 
+    expect(component).toBeInTheDocument();
+  });
 
-    const inputElement = getByLabelText('input-text');
-    fireEvent.change(inputElement, { target: { value: 'Hello World' } });
-    const newTitleElement = getByText(/Hello World/i);
-    expect(newTitleElement).toBeInTheDocument();
+  test('renders a title', () => {
+    const { queryByTestId } = render(<App />);
+    const title = queryByTestId('app-title');
+    expect(title).toBeInTheDocument();
+    expect(title.textContent).toBe('My App Component');
   });
 });
