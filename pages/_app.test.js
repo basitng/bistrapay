c4aa36a1920531e790e98c14e9544084c253469b
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:09:34 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,10 +1,27 @@
 import React from 'react';
-    import App from './_app';
+import {render, fireEvent} from '@testing-library/react';
 
-    describe('App component', () => {
-      it('should render', () => {
-        const { container } = render(<App />);
-        expect(container).not.toBeNull();
-        expect(container.firstChild).toMatchSnapshot();
-      });
-    });
+describe('The _app component', () => {
+  it('should render the page', () => {
+    const {getByText, getByLabelText} = render(
+      <_app />
+    );
+    const titleElement = getByText(/my page/i);
+    expect(titleElement).toBeInTheDocument();
+    const inputElement = getByLabelText('input-text');
+    expect(inputElement).toBeInTheDocument();
+   });
+  
+  it('should change page title when text is entered', () => {
+    const {getByText, getByLabelText} = render(
+      <_app />
+    );
+    const titleElement = getByText(/my page/i);
+    expect(titleElement).toBeInTheDocument();
+
+    const inputElement = getByLabelText('input-text');
+    fireEvent.change(inputElement, { target: { value: 'Hello World' } });
+    const newTitleElement = getByText(/Hello World/i);
+    expect(newTitleElement).toBeInTheDocument();
+  });
+});
