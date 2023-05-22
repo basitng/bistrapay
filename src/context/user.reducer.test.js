b9cc04c829e5e47a97c188f8c2ce82d446199288
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:45:38 +0100

    Updating unit test for user.reducer.js

--- src/context/user.reducer.test.js
+++ src/context/user.reducer.test.js
@@ -2,34 +2,40 @@ import React from 'react';
 import { userReducer } from './user.reducer';
 
 describe('userReducer', () => {
-  test('initial state', () => {
-    const expectedState = {
-      userName: '',
-      isLoggedIn: false
-    };
-    expect(userReducer(undefined, {})).toEqual(expectedState);
+  it('should return the initial state', () => {
+    expect(userReducer(undefined, {})).toEqual({
+      loading: false,
+      user: undefined,
+    });
   });
 
-  test('LOGIN', () => {
-    const action = {
-      type: 'LOGIN',
-      payload: { userName: 'John' }
-    };
-    const expectedState = {
-      userName: 'John',
-      isLoggedIn: true
-    };
-    expect(userReducer(undefined, action)).toEqual(expectedState);
+  it('should handle USER_REQUEST', () => {
+    expect(
+      userReducer({ loading: false, user: undefined }, { type: 'USER_REQUEST' })
+    ).toEqual({
+      loading: true,
+      user: undefined,
+    });
   });
-  
-  test('LOGOUT', () => {
-    const action = {
-      type: 'LOGOUT'
-    };
-    const expectedState = {
-      userName: '',
-      isLoggedIn: false
-    };
-    expect(userReducer(undefined, action)).toEqual(expectedState);
+
+  it('should handle USER_SUCCESS', () => {
+    expect(
+      userReducer(
+        { loading: true, user: undefined },
+        { type: 'USER_SUCCESS', payload: { name: 'John Doe' } }
+      )
+    ).toEqual({
+      loading: false,
+      user: { name: 'John Doe' },
+    });
+  });
+
+  it('should handle USER_FAILURE', () => {
+    expect(
+      userReducer({ loading: true, user: undefined }, { type: 'USER_FAILURE' })
+    ).toEqual({
+      loading: false,
+      user: undefined,
+    });
   });
 });
