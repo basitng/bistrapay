a4908323b4a449e5b8f8425f54f1d2a5e169afed
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:37:20 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
-import Manage from './manage';
+import { render, fireEvent } from '@testing-library/react';
+import Manage from './Manage';
 
-describe('Manage Component', () => {
-    it('should render the manage component', () => {
-        render(<Manage />);
-        expect(screen.getByText('Manage')).toBeInTheDocument();
-    });
+describe('Manage', () => {
+  it('should render the component', () => {
+    const { getByText } = render(<Manage />);
+    expect(getByText(/Manage/i)).toBeInTheDocument();
+  });
+
+  it('should render the expected components', () => {
+    const { getByText, getByLabelText, getByTestId } = render(<Manage />);
+    expect(getByText(/Manage/i)).toBeInTheDocument();
+    expect(getByLabelText(/Username/i)).toBeInTheDocument();
+    expect(getByLabelText(/Password/i)).toBeInTheDocument();
+    expect(getByTestId('submit-btn')).toBeInTheDocument();
+  });
+
+  it('should handle submit button click', () => {
+    const { getByText, getByLabelText, getByTestId } = render(<Manage />);
+    const usernameInput = getByLabelText(/Username/i);
+    const passwordInput = getByLabelText(/Password/i);
+    const submitBtn = getByTestId('submit-btn');
+
+    fireEvent.change(usernameInput, { target: { value: 'testUser' } });
+    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
+
+    fireEvent.click(submitBtn);
+
+    expect(getByText(/Success/i)).toBeInTheDocument();
+  });
 });
