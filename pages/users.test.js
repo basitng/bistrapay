fcb0193e2a809e3828d15856846a486153b1affe
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:38:25 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,33 +1,43 @@
 import React from 'react';
 
-const Users = () => {
-    return (
-        <div>
-            <h1>Users</h1>
-            <ul>
-                <li>User 1</li>
-                <li>User 2</li>
-                <li>User 3</li>
-            </ul>
-        </div>
-    )
-};
+const users = [
+    {name: "John", age: 30, email: "john@example.com"},
+    {name: "Mary", age: 25, email: "mary@example.com"}
+];
 
-export default Users;
+const UserList = () => (
+    <>
+        <h1>Users</h1>
+        <ul>
+            {users.map(user => (
+                <li key={user.name}>
+                    <span>Name: {user.name} </span>
+                    <span>Age: {user.age} </span>
+                    <span>Email: {user.email} </span>
+                </li>
+            ))}
+        </ul>
+    </>
+);
 
-// Unit Test for Users Component
+export default UserList;
+
+// Unit Test
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Users from './Users';
+import UserList from './users';
 
-test('Users component should render the header and a list of 3 users', () => {
-  render(<Users />);
+describe('UserList', () => {
+    test('should render the list of users', () => {
+        render(<UserList />);
+        const title = screen.getByText('Users');
+        const userNames = screen.getAllByText(/Name:/);
+        const userAges = screen.getAllByText(/Age:/);
+        const userEmails = screen.getAllByText(/Email:/);
 
-  // Assert that the header is rendered
-  expect(screen.getByText('Users')).toBeInTheDocument();
-
-  // Assert that the 3 users are rendered
-  expect(screen.getByText('User 1')).toBeInTheDocument();
-  expect(screen.getByText('User 2')).toBeInTheDocument();
-  expect(screen.getByText('User 3')).toBeInTheDocument();
+        expect(title).toBeInTheDocument();
+        expect(userNames).toHaveLength(2);
+        expect(userAges).toHaveLength(2);
+        expect(userEmails).toHaveLength(2);
+    });
 });
