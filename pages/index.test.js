a3f1cd712911dce0cf8e6b4466c222515ba4e8bf
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:09:53 +0100

    Updating unit test for index.js

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import {render} from '@testing-library/react';
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    );
-};
-
-describe('App', () => {
-    test('renders the component', () => {
-        const {getByText} = render(<App />);
-        expect(getByText('Hello World')).toBeInTheDocument();
+describe('index', () => {
+    it('renders the page', () => {
+        const { getByText } = render(<index />);
+        expect(getByText('Welcome to my page!')).toBeInTheDocument();
     });
 });
