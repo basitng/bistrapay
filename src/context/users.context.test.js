5179ab888c504325c67d96e1d462b629e9decd9a
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:20:32 +0100

    Updating unit test for users.context.js

--- src/context/users.context.test.js
+++ src/context/users.context.test.js
@@ -1,28 +1,85 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { UserContext } from './users.context';
+import React, { createContext, useReducer } from 'react';
 
-describe('UserContext component', () => {
-  it('should render its children', () => {
-    const { queryByText } = render(
-      <UserContext>
-        <div>Test</div>
-      </UserContext>
-    );
-    expect(queryByText('Test')).not.toBeNull();
+export const UserContext = createContext();
+
+const initialState = {
+  name: 'John Doe',
+  email: 'john@example.com',
+};
+
+export const userReducer = (state, action) => {
+  switch (action.type) {
+    case 'setName':
+      return { ...state, name: action.name };
+    case 'setEmail':
+      return { ...state, email: action.email }; 
+    default:
+      return state;
+  }
+};
+
+export const UserContextProvider = (props) => {
+  const [user, dispatch] = useReducer(userReducer, initialState);
+  return <UserContext.Provider value={{ user, dispatch }} {...props} />;
+};
+
+// Unit test
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { UserContextProvider, UserContext, userReducer } from './users.context';
+
+describe('Test user context reducer', () => {
+  it('Should set the name in the state', () => {
+    const action = {
+      type: 'setName',
+      name: 'Jane Doe',
+    };
+
+    const result = userReducer(initialState, action);
+
+    expect(result.name).toBe('Jane Doe');
   });
 
-  it('should update the context value when a button is clicked', () => {
-    const { queryByText } = render(
-      <UserContext>
-        <button>Change value</button>
-      </UserContext>
+  it('Should set the email in the state', () => {
+    const action = {
+      type: 'setEmail',
+      email: 'jane@example.com',
+    };
+
+    const result = userReducer(initialState, action);
+
+    expect(result.email).toBe('jane@example.com');
+  });
+});
+
+describe('Test user context provider', () => {
+  it('Should render  its children', () => {
+    render(
+      <UserContextProvider>
+        <div>Test</div>
+      </UserContextProvider>
     );
 
-    const button = queryByText('Change value');
-    expect(button).not.toBeNull();
+    expect(screen.getByText('Test')).toBeInTheDocument();
+  });
 
+  it('Should dispatch changes to the context state', () => {
+    const TestChild = () => {
+      const { dispatch } = React.useContext(UserContext);
+
+      return <button onClick={() => dispatch({ type: 'setName', name: 'John Doe' })}>Dispatch</button>;
+    };
+
+    render(
+      <UserContextProvider>
+        <TestChild />
+      </UserContextProvider>
+    );
+
+    const button = screen.getByText('Dispatch');
     fireEvent.click(button);
-    expect(UserContext.value).toBe('changed');
+
+    const { contacts } = React.useContext(UserContext);
+    expect(contacts.user.name).toBe('John Doe');
   });
 });
