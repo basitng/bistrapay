7aeab3480f8b63554ae41ee22bfed61eeda0d627
Basit Ng <basitng2004@gmail.com>
2023-05-22 12:09:27 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,34 +1,26 @@
 import React from 'react';
-import {render} from '@testing-library/react';
-import Manage from './Manage';
+import Manage from './manage';
 
-describe('Testing Manage Component', () => {
-    it('Should render the component', () => {
-        const {getByTestId} = render(<Manage />);
-        const manageComponent = getByTestId('manage-component');
-        expect(manageComponent).toBeInTheDocument();
-    });
+let component;
 
-    it('Should render all the fields correctly', () => {
-        const {getByTestId} = render(<Manage />);
-        const usernameField = getByTestId('username');
-        const passwordField = getByTestId('password');
-        const registerButton = getByTestId('register-button'); 
-        expect(usernameField).toBeInTheDocument();
-        expect(passwordField).toBeInTheDocument();
-        expect(registerButton).toBeInTheDocument();
-    });
+beforeEach(() => {
+  component = render(<Manage />);
+});
 
-    it('Should handle form submit correctly', () => {
-        const {getByTestId} = render(<Manage />);
-        const usernameField = getByTestId('username');
-        const passwordField = getByTestId('password');
-        const registerButton = getByTestId('register-button'); 
-        usernameField.value = 'Tutor';
-        passwordField.value = 'Testing123';
-        registerButton.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-        expect(usernameField.value).toBe('Tutor');
-        expect(passwordField.value).toBe('Testing123');
-    });
+describe('Manage Component', () => {
+  test('should render the component without errors', () => {
+    expect(component).toBeTruthy();
+  });
 
+  test('should check if the manage component has a form element', () => {
+    const manageForm = component.container.querySelector('form');
+    expect(manageForm).toBeTruthy();
+  });
+
+  test('should simulate a submit event on click of the submit button', () => {
+    const buttonElement = component.getByText('Submit');
+    fireEvent.click(buttonElement);
+    expect(buttonElement).toBeTruthy();
+  });
+ 
 });
