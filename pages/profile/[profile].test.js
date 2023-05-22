cecadebabcf6fc72c5d9da908567244bd6b42c60
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:36:00 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import Profile from './profile';
-
-describe('Profile', () => {
-  it('displays a display name when given', () => {
-    // Arrange
-    const displayName = 'John Doe';
-
-    // Act
-    const { getByText } = render(<Profile displayName={displayName} />);
-
-    // Assert
-    const displayNameElement = getByText(displayName);
-    expect(displayNameElement).toBeInTheDocument();
-  });
-
-  it('changes the display name when clicked', () => {
-    // Arrange
-    const initialDisplayName = 'John Doe';
-    const changedDisplayName = 'Jane Doe';
-
-    // Act
-    const { getByText, getByTestId } = render(<Profile displayName={initialDisplayName} />);
-
-    fireEvent.click(getByTestId('change-name-button'));
-
-    // Assert
-    const displayNameElement = getByText(changedDisplayName);
-    expect(displayNameElement).toBeInTheDocument();
-  });
-});
+    import { render } from '@testing-library/react';
+    
+    import Profile from './index';
+     
+    describe('Profile', () => {
+      test('it renders correctly', () => {
+        const { asFragment } = render(<Profile />);
+        expect(asFragment()).toMatchSnapshot();
+      });
+    });
