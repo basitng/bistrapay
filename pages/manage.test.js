67a67c779fd26d383de69c1493a96c0922d3dcdc
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:35:47 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,29 +1,34 @@
 import React from 'react';
+import {render} from '@testing-library/react';
+import Manage from './Manage';
 
-const Manage = () => {
-    return(
-        <div>
-            <h1>Manage</h1>
-            <p>Here you can manage the things from the list.</p>
-        </div>
-    );
-}
-
-export default Manage;
-
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Manage from './manage';
-
-describe('Manage', () => {
-    it('Navigates to the manage page', () => {
-        render(<Manage />);
-        expect(screen.getByText(/Manage/i)).toBeInTheDocument();
+describe('Testing Manage Component', () => {
+    it('Should render the component', () => {
+        const {getByTestId} = render(<Manage />);
+        const manageComponent = getByTestId('manage-component');
+        expect(manageComponent).toBeInTheDocument();
     });
 
-    it('Renders a header and a text block', () => {
-        render(<Manage />);
-        expect(screen.getByText('Manage')).toBeInTheDocument();
-        expect(screen.getByText('Here you can manage the things from the list.')).toBeInTheDocument();
+    it('Should render all the fields correctly', () => {
+        const {getByTestId} = render(<Manage />);
+        const usernameField = getByTestId('username');
+        const passwordField = getByTestId('password');
+        const registerButton = getByTestId('register-button'); 
+        expect(usernameField).toBeInTheDocument();
+        expect(passwordField).toBeInTheDocument();
+        expect(registerButton).toBeInTheDocument();
     });
+
+    it('Should handle form submit correctly', () => {
+        const {getByTestId} = render(<Manage />);
+        const usernameField = getByTestId('username');
+        const passwordField = getByTestId('password');
+        const registerButton = getByTestId('register-button'); 
+        usernameField.value = 'Tutor';
+        passwordField.value = 'Testing123';
+        registerButton.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+        expect(usernameField.value).toBe('Tutor');
+        expect(passwordField.value).toBe('Testing123');
+    });
+
 });
