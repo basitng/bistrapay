66bdbaf1bd48f70baede2c929af93763e0f4d887
Basit Ng <basitng2004@gmail.com>
2023-05-22 10:31:23 +0100

    Updating unit test for user.reducer.js

--- src/context/user.reducer.test.js
+++ src/context/user.reducer.test.js
@@ -1,61 +1,41 @@
-Test Example for user.reducer.js using React Testing Library
+import React from "react";
+import { render } from "@testing-library/react";
+import {UserReducer} from "../reducers/user.reducer";
 
-import { configure, shallow, mount } from 'enzyme';
-import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import UserReducer from './user.reducer';
-import { SET_USER_DATA, RESET_USER_DATA } from '../actionTypes';
-
-configure({ adapter: new Adapter() });
-
-// Test Component Rendering
-describe('UserReducer', () => {
-    it('should render correctly', () => {
-        const initialState = {
-            user: null
-        };
-        const wrapper = shallow(<UserReducer initialState={initialState} />);
-        expect(wrapper).toMatchSnapshot();
+describe('User Reducer', () => {
+  it('should return the initial state', () => {
+    expect(UserReducer(undefined, {})).toEqual({
+      user: null,
+      token: null
     });
-});
-
-// Test Props and State
-describe('UserReducer', () => {
-    it('should set user data correctly', () => {
-        const initialState = {
-            user: null
-        };
-        const wrapper = shallow(<UserReducer initialState={initialState} />);
-        const action = {
-            type: SET_USER_DATA,
-            payload: {
-                email: 'test@example.com',
-                name: 'Test User'
-            }
-        }
-        const newState = UserReducer(initialState, action);
-        expect(newState).toEqual({
-            user: {
-                email: 'test@example.com',
-                name: 'Test User'
-            }
-        });
+  });
+  it('should handle SET_USER', () => {
+    const payload = {
+      user: { name: 'John' },
+      token: 'testToken'
+    };
+    expect(
+      UserReducer(undefined, {
+        type: 'SET_USER',
+        payload
+      })
+    ).toEqual({
+      user: { name: 'John' },
+      token: 'testToken'
     });
-
-    it('should reset user data correctly', () => {
-        const initialState = {
-            user: {
-                email: 'test@example.com',
-                name: 'Test User'
-            }
-        };
-        const wrapper = shallow(<UserReducer initialState={initialState} />);
-        const action = {
-            type: RESET_USER_DATA
-        }
-        const newState = UserReducer(initialState, action);
-        expect(newState).toEqual({
-            user: null
-        });
+  });
+  it('should handle LOGOUT', () => {
+    expect(
+      UserReducer(
+        {
+          user: { name: 'John' },
+          token: 'testToken'
+        }, {
+          type: 'LOGOUT'
+        })
+    ).toEqual({
+      user: null,
+      token: null
     });
+  });
 });
