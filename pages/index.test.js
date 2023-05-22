8fc263664ac7aa79855abc5d9bd28180b7667305
Basit Ng <basitng2004@gmail.com>
2023-05-22 11:15:25 +0100

    Updating unit test for index.js

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import ActionButton from './ActionButton';
 
-describe('index', () => {
-    it('renders the page', () => {
-        const { getByText } = render(<index />);
-        expect(getByText('Welcome to my page!')).toBeInTheDocument();
-    });
+describe('ActionButton', () => {
+  it('should render the button', () => {
+    const { getByText } = render(<ActionButton label="Click Me" />);
+    expect(getByText('Click Me')).toBeInTheDocument();
+  });
+
+  it('should call the click handler', () => {
+    const clickHandler = jest.fn()
+    const { getByText } = render(<ActionButton label="Click Me" onClick={clickHandler} />);
+
+    fireEvent.click(getByText('Click Me'));
+    expect(clickHandler).toHaveBeenCalled();
+  });
 });
