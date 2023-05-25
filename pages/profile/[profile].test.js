87d7e2212ce2569bb82226a47239fa1dc914fcd8
Basit Ng <basitng2004@gmail.com>
2023-05-25 06:01:48 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,30 +1,18 @@
-//Here is an example using React Testing Library 
-
+import React from 'react';
 import { render } from '@testing-library/react';
-import Profile from './profile';
+import { Profile } from './profile';
 
-describe('Profile component', ()=> {
-  it('renders the profile page title', ()=> {
-    const { getByText } = render(<Profile />);
-    const titleElement = getByText(/profile/i);
-    expect(titleElement).toBeInTheDocument();
-  });
+describe('Profile', () => {
+    it('should render profile information', () => { 
+        const props = {
+            name: 'John Doe',
+            age: 30,
+            job: 'Software Developer'
+        }
+        const { getByText } = render(<Profile {...props} />);
 
-  it('should render the avatar with gravatar email address', () => {
-    const { getByTestId } = render(<Profile />);
-    const avatarElement = getByTestId('avatar');
-
-    expect(avatarElement.src).toBe('http://www.gravatar.com/test@email.com');
-  });
-
-  it('should render the profile details', ()  => {
-    const { getByText } = render(<Profile />);
-    const testNameElement = getByText('Name: John Doe');
-    const testCityElement = getByText('City: San Francisco');
-    const testBioElement = getByText('Bio: I am an experienced software developer.');
-
-    expect(testNameElement).toBeInTheDocument();
-    expect(testCityElement).toBeInTheDocument();
-    expect(testBioElement).toBeInTheDocument();
-  });
+        expect(getByText('Name: John Doe'));
+        expect(getByText('Age: 30'));
+        expect(getByText('Job: Software Developer'));
+    });
 });
