4a00cfc0425e71253c384cba496700ae3e9caa9f
Basit Ng <basitng2004@gmail.com>
2023-05-25 06:02:49 +0100

    Updating unit test for user.reducer.js

--- src/context/user.reducer.test.js
+++ src/context/user.reducer.test.js
@@ -1,63 +1,10 @@
-import { userReducer } from './user.reducer';
-    import {
-    LOGIN_USER_REQUEST, LOGIN_USER_FAILED, LOGIN_USER_SUCCESS
-    } from '../actions/user.action';
-
-import { act } from "@testing-library/react";
-
-describe("User Reducer", () => {
-  let state;
-
-  beforeEach(() => {
-    state = {
-      loading: false,
-      user: null,
-      error: "",
-    };
-  });
-
-  it("LOGIN_USER_REQUEST", () => {
-    const action = {
-      type: LOGIN_USER_REQUEST,
-    };
-    const result = userReducer(state, action);
-    expect(result).toEqual({
-      loading: true,
-      user: null,
-      error: "",
+import React from 'react';
+    
+    describe('UserReducer', () => {
+        it('should update user info', () => {
+            const user = {id: 1};
+            const action = {type: 'UPDATE_USER', payload:  'name'};
+            expect(UserReducer(user, action))
+                .toEqual({id: 1, name: 'name'})
+        });
     });
-  });
-
-  it("LOGIN_USER_FAILED", () => {
-    const payload = {
-      message: "error occuered",
-    };
-    const action = {
-      type: LOGIN_USER_FAILED,
-      payload,
-    };
-    const result = userReducer(state, action);
-    expect(result).toEqual({
-      loading: false,
-      user: null,
-      error: payload.message,
-    });
-  });
-
-  it("LOGIN_USER_SUCCESS", () => {
-    const payload = {
-      name: "John Doe",
-      email: "john@doe.com",
-    };
-    const action = {
-      type: LOGIN_USER_SUCCESS,
-      payload,
-    };
-    const result = userReducer(state, action);
-    expect(result).toEqual({
-      loading: false,
-      user: payload,
-      error: "",
-    });
-  });
-});
