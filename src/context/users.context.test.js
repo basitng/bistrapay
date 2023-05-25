38fa6e93cc6f265f42ea85602f2e67c33cfa35bc
Basit Ng <basitng2004@gmail.com>
2023-05-25 06:03:05 +0100

    Updating unit test for users.context.js

--- src/context/users.context.test.js
+++ src/context/users.context.test.js
@@ -1,55 +1,24 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { UserContext } from './users.context';
 
-import { UsersContext } from './users.context';
-
-let store;
-
-// Mock this function for testing
-const setUserName = jest.fn();
-const getUserName = jest.fn();
-
-beforeEach(() => {
-  store = {
-    userName: 'Bob',
-    setUserName,
-    getUserName
-  };
-});
-
-afterEach(cleanup);
-
-describe('UsersContext Component', () => {
-  it('renders the UsersContext provider with the store context', () => {
-    const { getByText } = render(
-      <UsersContext.Provider value={store}>
-        <div>Hello Bob!</div>
-      </UsersContext.Provider>
-    );
-    expect(getByText('Hello Bob!')).toBeInTheDocument();
+describe('UserContext', () => {
+  it('should render without crashing', () => {
+    const userContext = new UserContext();
+    expect(userContext).not.toBeNull();
   });
 
-  it('calls the setUserName when it is changed', () => {
-    const { getByTestId } = render(
-      <UsersContext.Provider value={store}>
-        <input type="text" id="userNameInput" data-testid="userNameInput" onChange={() => setUserName('Betty')} />
-      </UsersContext.Provider>
-    );
+  it('should have a method to add a user to the context', () => {
+    const userContext = new UserContext();
+    expect(userContext.addUser).toBeDefined();
+    expect(typeof userContext.addUser).toBe('function');
 
-    const input = getByTestId('userNameInput');
+    const user = {
+      id: 1,
+      name: 'John Smith',
+    };
+    userContext.addUser(user);
 
-    fireEvent.change(input, { target: { value: 'Betty' } });
-
-    expect(setUserName).toHaveBeenCalledWith('Betty');
-  });
-
-  it('calls the getUserName function', () => {
-    render(
-      <UsersContext.Provider value={store}>
-        <button onClick={() => getUserName()} />
-      </UsersContext.Provider>
-    );
-
-    expect(getUserName).toHaveBeenCalled();
+    // Check the value has changed
+    expect(userContext.users).toContain(user);
   });
 });
