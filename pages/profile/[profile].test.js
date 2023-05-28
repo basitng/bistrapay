dab8034779c7be6c48b8d3da001986e458d7179a
Basit Ng <basitng2004@gmail.com>
2023-05-28 06:07:24 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,18 +1,1 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Profile } from './profile';
-
-describe('Profile', () => {
-    it('should render profile information', () => { 
-        const props = {
-            name: 'John Doe',
-            age: 30,
-            job: 'Software Developer'
-        }
-        const { getByText } = render(<Profile {...props} />);
-
-        expect(getByText('Name: John Doe'));
-        expect(getByText('Age: 30'));
-        expect(getByText('Job: Software Developer'));
-    });
-});
+None
