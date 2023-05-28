2432e4b9956cbe8e02b512e5208e471dc78c6e90
Basit Ng <basitng2004@gmail.com>
2023-05-28 06:05:50 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,10 +1,1 @@
-//import React from 'react';
-import { render } from '@testing-library/react';
-import App from './_app';
-
-test('submit button should match the label', () => {
-  const { getByTestId } = render(<App />);
-  const buttonElement = getByTestId('submit-button');
-  
-  expect(buttonElement).toHaveTextContent('Submit');
-});
+None
