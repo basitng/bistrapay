9ab9487baaf6fc711d25c7a7c1a21a264cc22a73
Basit Ng <basitng2004@gmail.com>
2023-05-28 06:07:28 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,13 +1,1 @@
-// import React from 'react'
-
-import { render } from '@testing-library/react';
-
-describe('Users', () => {
-  it('should render the page with common elements', () => {
-    const { getByText } = render(<Users />);
-    expect(getByText('Users List')).toBeInTheDocument();
-    expect(getByText('Name')).toBeInTheDocument();
-    expect(getByText('Age')).toBeInTheDocument();
-    expect(getByText('Location')).toBeInTheDocument();
-  });
-});
+None
