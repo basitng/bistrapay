69391910766875f579b12f290dee84468b2a34ba
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:45:35 +0100

    Updating unit test for index.js

--- src/components/index.test.js
+++ src/components/index.test.js
@@ -1,1 +1,14 @@
-None
+import React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+
+import {Index} from './index';
+
+describe('Index component', () => {
+  afterEach(cleanup);
+
+  it('renders correctly', () => {
+    const { asFragment } = render(<Index />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
