ad49403172d8aa35488ceffd77d52aac6c200a7d
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:42:52 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./pages/_app";
-
-describe('App', () => {
-  
-  test("renders the App component", () => {
-    const {queryByTestId} = render(<App />);
-    expect(queryByTestId('app')).toBeInTheDocument();
-  });
-
-});
+// import React from 'react';
+    import App from './_app';
+ 
+    describe('App', () => {
+        let component;
+        beforeEach(() => {
+            component = shallow(<App />);
+        });
+    
+        it('should render the component properly', () => {
+            expect(component).toMatchSnapshot();
+        });
+    
+        it('should render one main element', () => {
+            expect(component.find('main').length).toBe(1);
+        });
+    
+        it('should render a footer', () => {
+            expect(component.find('footer').length).toBe(1);
+        });
+    });
