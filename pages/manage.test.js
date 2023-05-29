61cb8d13101a44f261f2634305eeec9487ba3327
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:23:24 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,30 +1,16 @@
-Answer: 
-// Import the React, Enzyme Library and whatever library you like
-import React from 'react'
-import {mount, shallow} from 'enzyme'
-import Manage from './pages/manage'
+// using None
+    // Unit Test
+    import { shallow, mount } from "enzyme";
+    import ManagePage from "./pages/manage";
 
-// Unit test for Manage component
-describe('Manage component', () => {
-    // Initializing the component
-    let wrapper;
-    beforeEach(() => {
-        wrapper = shallow(<Manage />);
+    describe('ManagePage Component', () => {
+      it('should render a "div" element that contains the text "manage page"', () => {
+        const wrapper = shallow(<ManagePage />);
+        expect(wrapper.find('div').text()).toBe('manage page');
+      });
+
+      it('should have a "content" div that has a "className" of "content"', () => {
+        const wrapper = mount(<ManagePage />);
+        expect(wrapper.find('.content').length).toEqual(1);
+      });
     });
-
-    //Test to ensure the component renders correctly
-    it('should render Manage component correctly', () => {
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    // Test to render the component
-    it('should render', () => {
-        expect(wrapper.exists).toBeTruthy();
-    });
-
-    // Test to find if the component renders the correct HTML element
-    it('should render a div', () => {
-        expect(wrapper.find('div').length).toEqual(1);
-    });
-
-});
