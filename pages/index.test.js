9a4b36fecec02d16df97499daebebe29740dd52a
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:43:31 +0100

    Updating unit test for index.js

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,99 +1,26 @@
-import React, { useState, useEffect } from "react";
-import { useForm } from 'react-hook-form';
+import React from 'react';
+import Button from './Button';
 
-function App() {
-    const [userInput, setUserInput] = useState("");
-    const [data, setData] = useState([]);
-    const { register, handleSubmit } = useForm();
-    
-    useEffect(() => {
-        fetch('/endpoint')
-        .then(res => res.json())
-        .then(data => setData(data));
-    }, []);
-    
-    const onSubmit = async (data) => {
-        const res = await fetch('/endpoint', {
-            method: 'post',
-            body: JSON.stringify(data),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-        });
-        if (res.ok) {
-            setUserInput("");
-            const newData = await res.json();
-            setData([...data, newData])
-        }
-    }
-    
+const IndexPage = () => {
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input
-                    type="text"
-                    name="input"
-                    value={userInput}
-                    ref={register}
-                    onChange={e => {setUserInput(e.target.value)}}
-                />
-            <input type="submit" value="Submit" />
-            </form>
-            <div>
-                {data.map(item => (
-                    <div key={item.id}>{item.name}</div>
-                ))}
-            </div>
+            <h1>Index Page</h1>
+            <Button label="click me" />
         </div>
-    )
+    );
 }
 
-export default App;
+export default IndexPage;
 
-None:
-import { shallow } from 'enzyme';
-import App from './App';
+//Unit Test
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import IndexPage from './index';
 
-describe('App', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<App />);
-  });
+afterEach(cleanup);
 
-  it('should update userInput state on change of input value', () => {
-    wrapper.find('input[name="input"]').simulate('change', {
-      target: {
-        value: 'user input'
-      }
-    });
-    expect(wrapper.find('input[name="input"]').prop('value')).toBe('user input');
-  });
-
-  it('should make a post request when form is submitted', () => {
-    wrapper.find('form').simulate('submit');
-    expect(fetch).toHaveBeenCalledWith('/endpoint', {
-      method: 'post',
-      body: JSON.stringify(''),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  });
-
-  it('should update data state when post request is successful', async () => {
-    wrapper.find('form').simulate('submit', {
-      name: 'name'
-    });
-    const res = {
-      ok: true,
-      json: () => ({
-        name: 'name',
-        id: 'id'
-      })
-    };
-    await expect(fetch).resolves.toEqual(res);
-    expect(wrapper.find('div').first().prop('children')).toEqual([
-      'name',
-    ]);
-  });
+test('it should render the <IndexPage /> correctly', () => {
+    const { getByText } = render(<IndexPage />);
+    expect(getByText('IndexPage')).toBeInTheDocument();
+    expect(getByText('click me')).toBeInTheDocument();
 });
