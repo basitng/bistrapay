3c8ecea269113efb56ebbf4ed007b12b0a899b4a
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:31:51 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,16 +1,9 @@
-// using None
-    // Unit Test
-    import { shallow, mount } from "enzyme";
-    import ManagePage from "./pages/manage";
-
-    describe('ManagePage Component', () => {
-      it('should render a "div" element that contains the text "manage page"', () => {
-        const wrapper = shallow(<ManagePage />);
-        expect(wrapper.find('div').text()).toBe('manage page');
-      });
-
-      it('should have a "content" div that has a "className" of "content"', () => {
-        const wrapper = mount(<ManagePage />);
-        expect(wrapper.find('.content').length).toEqual(1);
-      });
+import React from 'react'; 
+    import { shallow } from 'enzyme'; 
+    import Manage from './Manage'; 
+    
+    describe('Manage Component', () => { 
+        it('should render without crashing', () => { 
+            shallow(<Manage />);
+        }); 
     });
