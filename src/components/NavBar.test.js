4ff2cec901939f16066ae37568dd4f7543b30b6b
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:45:23 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,1 +1,28 @@
-None
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import NavBar from '../components/NavBar';
+
+describe('Nav Bar Component', () => {
+    it('should change the navigation bar on click ', () => {
+        //Arrange
+        const navBarItems = [{
+            title: 'Home',
+            path: '/home'
+        }, {
+            title: 'Dashboard',
+            path: '/dashboard'
+        }]
+
+        const { getByTestId } = render(<NavBar navBarItems={navBarItems} />);
+
+        //Act
+        const homeNavButton = getByTestId('navbar-home');
+        fireEvent.click(homeNavButton);
+        const dashboardNavButton = getByTestId('navbar-dashboard');
+        fireEvent.click(dashboardNavButton);
+
+        //Assert
+        expect(getByTestId('navbar-home')).not.toHaveClass('active');
+        expect(getByTestId('navbar-dashboard')).toHaveClass('active');
+    });
+});
