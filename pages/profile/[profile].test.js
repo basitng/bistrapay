81b6216b3d4e77f9e66c2a8ade26becf337662b1
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:44:24 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,1 +1,23 @@
-None
+import React from 'react';
+    
+    import Profile from './index';
+    
+    import { render, cleanup } from 'react-testing-library';
+    
+    afterEach(cleanup);
+    
+    describe('<Profile />', ()=>{
+        it('should render without errors', ()=>{
+            render(<Profile />);
+        })
+    
+        it('should contain the correct name and profile picture', ()=>{
+            const { getByText, getByTestId } = render(<Profile name="name" imgUrl="picture.png"/>);
+    
+            const nameNode = getByText('name');
+            expect(nameNode).toBeTruthy();
+    
+            const profilePictureNode = getByTestId('profile-picture')
+            expect(profilePictureNode).toHaveAttribute('src', 'picture.png');
+        })
+    })
