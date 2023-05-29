2ba505837f22784097c0d29b66844a01cc2a183d
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:43:59 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,9 +1,29 @@
-import React from 'react'; 
-    import { shallow } from 'enzyme'; 
-    import Manage from './Manage'; 
-    
-    describe('Manage Component', () => { 
-        it('should render without crashing', () => { 
-            shallow(<Manage />);
-        }); 
-    });
+import React from 'react';
+     
+     const Manage = () => {
+        const [title, setTitle] = React.useState(""); 
+        const updateTitle = () => {
+            setTitle("Updated Title");
+        }
+
+        return (
+            <div>
+               <button onClick={updateTitle}>Update Title</button>
+               <p>{title}</p>
+            </div>
+        )
+     }
+     
+     // Unit Test
+     import React from 'react';
+     import { render, fireEvent, screen } from '@testing-library/react';
+     
+     test('updates the title correctly when the updateTitle button is clicked', () => {
+        const { container } = render(<Manage />);
+        const updateTitleButton = screen.getByText("Update Title");
+        const titleElement = screen.getByText(/^$/)
+     
+        expect(titleElement.textContent).toBe('');
+        fireEvent.click(updateTitleButton);
+        expect(titleElement.textContent).toBe('Updated Title');
+     });
