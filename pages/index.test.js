020b963af45e09a890b3644210b42dceca6557f0
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:31:35 +0100

    Updating unit test for index.js

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,15 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useForm } from 'react-hook-form';
+
+function App() {
+    const [userInput, setUserInput] = useState("");
+    const [data, setData] = useState([]);
+    const { register, handleSubmit } = useForm();
+    
+    useEffect(() => {
+        fetch('/endpoint')
+        .then(res => res.json())
+        .then(data => setData(data));
+    }, []);
+    
+    const onSubmit = async (data) => {
+        const res = await fetch('/endpoint', {
+            method: 'post',
+            body: JSON.stringify(data),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        });
+        if (res.ok) {
+            setUserInput("");
+            const newData = await res.json();
+            setData([...data, newData])
+        }
+    }
+    
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    type="text"
+                    name="input"
+                    value={userInput}
+                    ref={register}
+                    onChange={e => {setUserInput(e.target.value)}}
+                />
+            <input type="submit" value="Submit" />
+            </form>
+            <div>
+                {data.map(item => (
+                    <div key={item.id}>{item.name}</div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default App;
+
+None:
 import { shallow } from 'enzyme';
-import Index from './index';
+import App from './App';
 
-describe('Index', () => {
-    it('should render a div', () => {
-        const wrapper = shallow(<Index />);
-        expect(wrapper.find('div').length).toEqual(1);
-    });
+describe('App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
 
-    it('should contain the correct props', () => {
-        const wrapper = shallow(<Index title="Test" />);
-        expect(wrapper.instance().props.title).toEqual('Test');
+  it('should update userInput state on change of input value', () => {
+    wrapper.find('input[name="input"]').simulate('change', {
+      target: {
+        value: 'user input'
+      }
     });
+    expect(wrapper.find('input[name="input"]').prop('value')).toBe('user input');
+  });
+
+  it('should make a post request when form is submitted', () => {
+    wrapper.find('form').simulate('submit');
+    expect(fetch).toHaveBeenCalledWith('/endpoint', {
+      method: 'post',
+      body: JSON.stringify(''),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  });
+
+  it('should update data state when post request is successful', async () => {
+    wrapper.find('form').simulate('submit', {
+      name: 'name'
+    });
+    const res = {
+      ok: true,
+      json: () => ({
+        name: 'name',
+        id: 'id'
+      })
+    };
+    await expect(fetch).resolves.toEqual(res);
+    expect(wrapper.find('div').first().prop('children')).toEqual([
+      'name',
+    ]);
+  });
 });
