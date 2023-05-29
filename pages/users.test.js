eac66098171615edbbf3188d20dc938510d655c7
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:23:50 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,1 +1,14 @@
-None
+//using Jest test library
+        describe("User page", () => { 
+            test("page renders correctly", () => { 
+                const wrapper = shallow(<Users />);
+                expect(wrapper).toMatchSnapshot();
+             }); 
+        }); 
+
+        describe("User page", () => { 
+            test("User component rendered without error", () => { 
+                const component = shallow(<Users />);
+                expect(component.find("User").length).toEqual(1);
+            }); 
+        });
