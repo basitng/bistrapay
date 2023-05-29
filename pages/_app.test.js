25381e3dcd3c6d6221596d555a7afda74f1ee9ad
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:37:44 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,16 +1,35 @@
 import React from 'react';
 
-        function App() {
-            return <div> Hello World! </div>;
-        }
-        
-        // Unit Test
-        import { shallow } from 'enzyme';
-        import App from './_app';
-        
-        describe('App Component', () => {
-            it('should render correctly', () => {
-                const wrapper = shallow(<App />);
-                expect(wrapper).toMatchSnapshot();
-            });
+    function MyApp(){
+        const [name, setName] = React.useState('John');
+
+        const changeName = () =>
+            setName(name === 'John' ? 'Paul' : 'John');
+
+        return (
+            <div>
+                <h2>{name}</h2>
+                <button onClick={changeName}>Change Name</button>
+            </div>
+        );
+    }
+
+    // Unit Test
+    import React from "react";
+    import { render, fireEvent } from "@testing-library/react";
+    import MyApp from "./_app";
+    
+    describe("MyApp Component", () => {
+        it("should test name state and onclick button functionality", () => {
+            const { getByText, getByTestId } = render(<MyApp />);
+
+            // Content should show John initially
+            expect(getByTestId("name-state").textContent).toBe("John");
+
+            // Click the button to change the state to Paul
+            fireEvent.click(getByText("Change Name"));
+
+            // Content should show Paul after change
+            expect(getByTestId("name-state").textContent).toBe("Paul");
         });
+    });
