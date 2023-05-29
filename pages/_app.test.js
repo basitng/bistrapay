74f21fc9c703e087b7e5d791ea276707351bf125
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:34:57 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,1 +1,16 @@
-Answer: None
+import React from 'react';
+
+        function App() {
+            return <div> Hello World! </div>;
+        }
+        
+        // Unit Test
+        import { shallow } from 'enzyme';
+        import App from './_app';
+        
+        describe('App Component', () => {
+            it('should render correctly', () => {
+                const wrapper = shallow(<App />);
+                expect(wrapper).toMatchSnapshot();
+            });
+        });
