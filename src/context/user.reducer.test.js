67f5e6726cd4bbf780b8d119b21a9339c7ed1826
Basit Ng <basitng2004@gmail.com>
2023-05-29 14:24:58 +0100

    Updating unit test for user.reducer.js

--- src/context/user.reducer.test.js
+++ src/context/user.reducer.test.js
@@ -1,10 +1,56 @@
-import React from 'react';
-    
-    describe('UserReducer', () => {
-        it('should update user info', () => {
-            const user = {id: 1};
-            const action = {type: 'UPDATE_USER', payload:  'name'};
-            expect(UserReducer(user, action))
-                .toEqual({id: 1, name: 'name'})
-        });
+import { userReducer, initialState } from './user.reducer';
+import {
+    updateUsername,
+    updateEmail,
+    updatePassword
+} from './user.actions';
+
+// Set up test library or framework
+describe('userReducer', () => {
+    it('should return initial state', () => {
+        expect(userReducer(undefined, {}))
+            .toEqual(initialState);
     });
+
+    it('should handle updateUsername action', () => {
+        const action = {
+            type: updateUsername,
+            payload: 'John Smith'
+        };
+        const expectedState = {
+            ...initialState,
+            username: 'John Smith'
+        };
+
+        expect(userReducer(undefined, action))
+            .toEqual(expectedState);
+    });
+
+    it('should handle updateEmail action', () => {
+        const action = {
+            type: updateEmail,
+            payload: 'john.smith@example.com'
+        };
+        const expectedState = {
+            ...initialState,
+            email: 'john.smith@example.com'
+        };
+
+        expect(userReducer(undefined, action))
+            .toEqual(expectedState);
+    });
+
+    it('should handle updatePassword action', () => {
+        const action = {
+            type: updatePassword,
+            payload: '123456'
+        };
+        const expectedState = {
+            ...initialState,
+            password: '123456'
+        };
+
+        expect(userReducer(undefined, action))
+            .toEqual(expectedState);
+    });
+});
