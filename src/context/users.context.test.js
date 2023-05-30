08009e92399f06e3d73b3e823e1b3e497990dd1e
Basit Ng <basitng2004@gmail.com>
2023-05-30 23:14:48 +0100

    Updating unit test for users.context.js

--- src/context/users.context.test.js
+++ src/context/users.context.test.js
@@ -1,14 +1,1 @@
-import { mockUserData } from "../../test/mockData/users/mockUserData";
-    import { UsersContext } from "./users.context";
-
-    describe("UsersContext unit testing", () => {
-      it("Should return an object with 'userData' key", () =>{
-        const contextValue = UsersContext();
-        expect(contextValue).toHaveProperty("userData");
-      });
-
-      it("Should return an object containing the mockUserData", () => {
-        const contextValue = UsersContext();
-        expect(contextValue.userData).toEqual(mockUserData);
-      });
-    });
+None
