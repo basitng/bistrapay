89e661d56c6f6de241f5fe964853c5347c8f5f90
Basit Ng <basitng2004@gmail.com>
2023-05-30 18:13:51 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,17 +1,27 @@
 import React from 'react';
-        import { shallow, mount } from 'enzyme';
-        import Manage from '../../pages/Manage';
-        
-        describe('Manage Page', () => {
-          it('renders without crashing', () => {
-            shallow(<Manage />);
-          });
-        
-          it('renders content crrectly', () => {
-            const managePage = mount(<Mange />);
-            expect(managePage.find(".manage__title").text()).toEqual("Manage Page");
-            expect(managePage.find('#manage__add').text()).toEqual("Add");
-            expect(managePage.find('#manage__remove').text()).toEqual("Remove");
-          });
-          
-        });
+    import { render, fireEvent } from '@testing-library/react';
+    import ManagePage from './ManagePage';
+    
+    describe('Manage Page', () => {
+      it('shows the page correctly', () => {
+        const { container } = render(<ManagePage />);
+        expect(container.innerHTML).toMatch('<h1>Manage Page</h1>');
+        expect(container.querySelector('button')).toHaveTextContent('Create');
+      });
+    
+      it('form fields are working correctly', () => {
+        const { getByLabelText} = render(<ManagePage />);
+        const jobTitleInput = getByLabelText('Job Title');
+        fireEvent.change(jobTitleInput, { target: { value: 'Software Engineer' } });
+        expect(jobTitleInput.value).toBe('Software Engineer');
+      });
+    
+      it('creates successfully', () => {
+        const { getByText, getByLabelText } = render(<ManagePage />);
+        const jobTitleInput = getByLabelText('Job Title');
+        fireEvent.change(jobTitleInput, { target: { value: 'Software Engineer' } });
+        const createBtn = getByText('Create');
+        fireEvent.click(createBtn);
+        expect(getByText('Successfully created a job!')).toBeInTheDocument();
+      });
+    });
