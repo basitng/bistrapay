41611391765f493a41392c1481ef800a86b252dc
Basit Ng <basitng2004@gmail.com>
2023-05-30 18:14:58 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,24 +1,28 @@
-const { render, fireEvent } = render(<User />);
-    
-    //Test 1: test email input
-    const emailidentifier =  'input#email';
-    //find the email input element
-    const emailInputElement = container.querySelector(emailidentifier);
-    //simulate a value to be entered 
-    fireEvent.change(emailInputElement, {
-        target: { value: 'test@example.com' },
+import React from 'react';
+ 
+function UsersList({ users }) {
+    return (
+        <ul className="users-list">
+            {users.map((user, index) => (
+                <li key={index}>{user.name}</li>
+            ))}
+        </ul>
+    );
+}
+
+// Test using react-testing-library
+import {render, screen} from '@testing-library/react'
+ 
+describe('UsersList Component Test', () => {
+    test('Check users list is rendered correctly', () => {
+        const mockUsers = [{name: 'John Doe'}, {name: 'Tim Smith'}];
+ 
+        render(<UsersList users={mockUsers} />);
+ 
+        const usersItems = screen.getAllByTestId('user-item');
+ 
+        expect(usersItems.length).toBe(mockUsers.length);
+        expect(usersItems[0].innerHTML).toBe(mockUsers[0].name);
+        expect(usersItems[1].innerHTML).toBe(mockUsers[1].name);
     });
-    //check that the value is really entered in the field
-    expect(emailInputElement.value).toBe('test@example.com');
-    
-    
-    //Test 2: test password input
-    const passwordIdentifier =  'input#password';
-    //find the password input element
-    const passwordInputElement = container.querySelector(passwordIdentifier);
-    //simulate a value to be entered 
-    fireEvent.change(passwordInputElement, {
-        target: { value: 'testpassword' },
-    });
-    //check that the value is really entered in the field
-    expect(passwordInputElement.value).toBe('testpassword');
+});
