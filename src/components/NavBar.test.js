3e8b3251477a836e6bca5fa279257d8664ac5146
Basit Ng <basitng2004@gmail.com>
2023-05-30 23:03:53 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,12 +1,19 @@
-//import React from 'react';
-    //import NavBar from './NavBar';
-    //import { render } from '@testing-library/react';
+//Use the React Testing Library
+    import React from 'react';
+    import {render, fireEvent, screen} from "@testing-library/react"
+    import NavBar from './NavBar';
     
-    describe('Testing NavBar component', () => {
-        test('NavBar renders correctly', () => { 
-            const { getByTestId } = render(<NavBar />);
-            expect(getByTestId('navbar')).toBeInTheDocument(); 
-            expect(getByTestId('navbar-element-a')).toBeInTheDocument();
-            expect(getByTestId('navbar-element-b')).toBeInTheDocument();
+    describe('NavBar Component', () => {
+        it('should render the NavBar correctly', () => {
+            const {getByRole} = render(<NavBar />);
+            expect(getByRole("navigation").children.length).toBe(3)
         });
-    });
+
+        it("should call the onChange when the input changes", () => {
+            const mockHandleChange = jest.fn()
+            const {getByRole} = render(<NavBar onChange={mockHandleChange}/>)
+            const input = getByRole("textbox")
+            fireEvent.change(input, {target: {value: "This is a test"}})
+            expect(mockHandleChange).toHaveBeenCalledTimes(1)
+        })
+    })
