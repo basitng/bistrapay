ab5c0467b2fee5f2724a08710ecdd47aef5ce3a4
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:39:44 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,23 +1,11 @@
-import React from 'react'
-
-export const Profile = (props) => {
-    const { name } = props;
-
-    return (
-        <div>
-            <h1>Profile of {name}</h1>
-            <p>This is the profile page of {name}.</p>
-        </div>
-    )
-};
-
-import { render, screen } from '@testing-library/react';
-import { Profile } from './pages/profile/[profile].js'
- 
-describe('Profile', () => {
-  it('should render the name in the h1 tag', () => {
-    const name = 'John';
-    render(<Profile name={name} />);
-    expect(screen.getByText(/John/)).toBeInTheDocument()
-  });
-});
+import React from 'react';
+    import { shallow } from 'enzyme';
+    import Profile from './profile';
+    
+    describe('Profile component', () => {
+        it('should render without errors', () => {
+            const component = shallow(<Profile />);
+            const wrapper = component.find('.component');
+            expect(wrapper.length).toBe(1);
+        }); 
+    });
