9333c5a2e4cb20780725ee4f0d04eb43045f6676
Basit Ng <basitng2004@gmail.com>
2023-05-30 14:02:36 +0100

    Updating unit test for hello.js

--- pages/api/hello.test.js
+++ pages/api/hello.test.js
@@ -1,16 +1,39 @@
 import React from 'react';
+    import {render, fireEvent } from '@testing-library/react';
+    import Hello from './hello';
 
-const Hello = () => {
-  return <h1>Hello!</h1>;
-};
+    describe('Hello', () => {
+        it('should display a greeting', () => {
+            //Arrange
+            const name = 'Dan';
+            const title = 'Developer';
 
-export default Hello;
+            const { getByText } = render(<Hello name={name} title={title} />)
+             
+            // Act
+            const element = getByText(/hello/i);
 
-import {render, screen} from '@testing-library/react';
+            // Assert
+            expect(element).toBeInTheDocument();
+            expect(element).toHaveTextContent('Hello Dan! You are a Developer');
+        });
 
-describe('Hello', () => {
-  it('should render hello', () => {
-    render(<Hello />);
-    expect(screen.getByText('Hello!')).toBeInTheDocument();
-  });
-});
+        it('the text should change when a new name is provided', () => {
+            //Arrange
+            const name = 'Test';
+            const title = 'Tester';
+
+            const { getByText, getByLabelText } = render(<Hello name={name} title={title} />)
+
+            const nameInput = getByLabelText(/name/i);
+            const titleInput = getByLabelText(/title/i);
+
+            // Act
+            fireEvent.change(nameInput, { target: { value: 'Test' } });
+            fireEvent.change(titleInput, { target: { value: 'Tester' } });
+
+            // Assert
+            const element = getByText('Hello Test! You are a Tester');
+            expect(element).toBeInTheDocument();
+        });
+    });
