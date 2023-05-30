4f11be0a6ca48de6a1c62bedfc9a7d6223dadd2a
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:44:37 +0100

    Updating unit test for users.context.js

--- src/context/users.context.test.js
+++ src/context/users.context.test.js
@@ -1,54 +1,11 @@
 import React from 'react';
-import { UserContext } from './users.context';
+        import { mount, shallow } from 'enzyme';
+        import { UsersContext } from '../context/users.context';
+        
+        describe('Component rendering', () => {
 
-const UserProvider = ({ children }) => {
-  const [users, setUsers] = React.useState([]);
-
-  const getUsers = async () => {
-    const response = await fetch('/api/users');
-    setUsers(response.data);
-  };
-
-  return (
-    <UserContext.Provider
-      value={{
-        users,
-        getUsers,
-      }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
-};
-
-export { UserProvider };
-
-
-import { render, fireEvent, act } from '@testing-library/react';
-import { UserProvider } from './users.context';
-
-describe('UserContext', () => {
-  it('should get users', async () => {
-    jest.spyOn(global, 'fetch').mockImplementation(() =>
-      Promise.resolve({
-        json: () => {
-          return Promise.resolve([{
-            name: 'John Doe'
-          }])
-        }
-      })
-    );
-  
-    const { getByTestId } = render(
-      <UserProvider>
-        <div data-testid="user-name">John Doe</div>
-      </UserProvider>
-    );
-  
-    await act(async () => {
-      await fireEvent.click(getByTestId('user-name'));
-    });
-  
-    expect(global.fetch).toHaveBeenCalled();
-  });
-});
+           it('should render correctly', () => {
+              const wrapper = shallow(<UsersContext.Provider>Test</UsersContext.Provider>);
+              expect(wrapper).toMatchSnapshot();
+           });
+        });
