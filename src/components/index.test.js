9afd85bd69e70880722990034471d9785bd8711b
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:58:31 +0100

    Updating unit test for index.js

--- src/components/index.test.js
+++ src/components/index.test.js
@@ -1,16 +1,1 @@
-import React from 'react'
-    import ReactDOM from 'react-dom'
-    import { App } from './App';
-    import { shallow } from 'enzyme' 
-
-    describe('App', () => {
-      let wrapper; 
-      beforeEach(() => {
-        wrapper = shallow(<App />)
-      })
-  
-      it('should render properly', () => {
-        expect(<App />).toBeDefined()
-        expect(<App />).toMatchSnapshot()
-      })
-    })
+None
