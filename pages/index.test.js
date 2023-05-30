aee1fade7db950f97a9d0ed9e01b988d3f4d7484
Basit Ng <basitng2004@gmail.com>
2023-05-30 14:03:03 +0100

    Updating unit test for index.js

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,51 +1,20 @@
-import React from "react";
- 
-const DisplayComponent = () => {
-  const [state, setState] = React.useState({
-    name: '',
-    isLoading: false
-  });
- 
-  const handleClick = () => {
-    setState({ isLoading: true, name: state.name });
-  }; 
- 
-  return (
-    <div>
-      <input
-        type="text"
-        value={state.name}
-        onChange={event => setState({ name: event.target.value })}
-      />
-      <button onClick={handleClick}>Submit</button>
-      {state.isLoading && (
-        <div>
-          {state.name} is loading...
-        </div>
-      )}
-    </div>
-  );
-};
- 
-export default DisplayComponent;
-
-// Unit test for the above code using React-testing-library
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import DisplayComponent from './DisplayComponent';
-
-describe('Display Component', () => {
-  it('Should set loading to true when button is clicked', () => {
-    const { getByRole } = render(<DisplayComponent />);
-    const input = getByRole('textbox');
-    const submitButton = getByRole('button');
-
-    fireEvent.change(input, { target: { value: 'hello' } });
-
-    expect(input.value).toBe('hello');
-    fireEvent.click(submitButton);
-    expect(getByRole('textbox').value).toBe('hello')
-    expect(getByRole('textbox').value).toBe('hello is loading...')
-  });
-
-});
+//Solution
+        import React from 'react';
+        import {render, fireEvent} from '@testing-library/react';
+        import App from './pages/index';
+        
+        describe('Index Page', () => {
+            it('Should render correctly', () => {
+                const { getByTestId } = render(<App />);
+                const pageTitle = getByTestId('page-title');
+                expect(pageTitle).toHaveTextContent('Welcome!');
+            });
+        
+            it('Should change page title on button click', () => {
+                const { getByTestId } = render(<App />);
+                const button = getByTestId('change-title-button');
+                fireEvent.click(button);
+                const pageTitle = getByTestId('page-title');
+                expect(pageTitle).toHaveTextContent('Page Title Changed!');
+            });
+        });
