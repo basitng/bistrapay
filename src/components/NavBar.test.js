60b76ee769a55bc6188ca87948bb9b1d46a83fc5
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:58:27 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,28 +1,12 @@
-describe('NavBar', () => {
-  let wrapper;
-  
-  beforeEach(() => {
-    wrapper = shallow(<NavBar />);
-  });
-
-  it('should render without error', () => {
-    const navbarComponent = wrapper.find(".nav-container");
-    expect(navbarComponent.length).toBe(1);
-  });
-  
-  it('should render a logo', () => {
-    const logoImg = wrapper.find(".logo");
-    expect(logoImg.length).toBe(1);
-  });
-  
-  it('should render a searchbar', () => {
-    const searchbar = wrapper.find('.searchbar');
-    expect(searchbar.length).toBe(1);
-  });
-  
-  it('should render menu items', () => {
-    const menuItemList = wrapper.find('.menu-item');
-    expect(menuItemList.length).toBeGreaterThan(0);
-  });
-  
-  });
+//import React from 'react';
+    //import NavBar from './NavBar';
+    //import { render } from '@testing-library/react';
+    
+    describe('Testing NavBar component', () => {
+        test('NavBar renders correctly', () => { 
+            const { getByTestId } = render(<NavBar />);
+            expect(getByTestId('navbar')).toBeInTheDocument(); 
+            expect(getByTestId('navbar-element-a')).toBeInTheDocument();
+            expect(getByTestId('navbar-element-b')).toBeInTheDocument();
+        });
+    });
