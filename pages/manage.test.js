2853c077da24f454d6485b6117e0a761ce11d3e2
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:40:57 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,1 +1,10 @@
-None
+import React from 'react';
+import { shallow } from 'enzyme';
+import Manage from './Manage';
+
+describe('Manage component', () => {
+    it('should render without crashing', () => {
+        const wrapper = shallow(<Manage />);
+        expect(wrapper).toBeDefined();
+    }); 
+});
