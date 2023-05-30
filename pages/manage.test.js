746c15a7c5350ff1cd3ec80a653df4a5b6fd5bef
Basit Ng <basitng2004@gmail.com>
2023-05-30 13:36:28 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,15 +1,58 @@
 import React from 'react';
-    import { render, cleanup } from 'react-testing-library';
-    
-    afterEach(cleanup);
-    
-    const Manage = () => <div><h1>Manage</h1><section>Manage Content</section></div>;
-    
-    describe('Manage', () => {
-      it('should render manage component and content correctly', () => {
-        const { getByTestId, container } = render(<Manage />);
-    
-        expect(getByTestId('manage')).toBeTruthy();
-        expect(container).toMatchSnapshot();
-      });
+
+class ManagePage extends React.Component {
+
+  state = {
+    text: '',
+    isActive: false
+  };
+
+  handleTextChange = (e) => {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmitForm = () => {
+    if (this.state.text.trim().length > 4) {
+      this.setState({ isActive: true });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="form">
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleTextChange}
+          />
+        </div>
+        <button onClick={this.handleSubmitForm} disabled={!this.state.isActive}>
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ManagePage;
+
+
+import React from 'react';
+import { render, fireEvent, act } from '@testing-library/react';
+
+describe('ManagePage', () => {
+    it('should enable the submit button when enough characters have been entered', () => {
+        const { getByText, container, getByPlaceholderText } = render(<ManagePage />);
+        const input = getByPlaceholderText("Enter text...");
+        const submitButton = getByText('Submit');
+
+        expect(submitButton.disabled).toEqual(true);
+
+        act(() => {
+            fireEvent.change(input, { target: { value: 'test' } });
+        });
+        expect(input.value).toEqual('test');
+        expect(submitButton.disabled).toEqual(false);
     });
+});
