4135c41afc782af4c19aef08831f637fe52de369
Basit Ng <basitng2004@gmail.com>
2023-05-30 12:21:50 +0100

    Updating unit test for hello.js

--- pages/api/hello.test.js
+++ pages/api/hello.test.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const Hello = () => {
-    return <div>Hello World</div>;
-};
+function Hello() {
+  return <div>Hello world!</div>;
+}
 
 export default Hello;
 
+// Unit test for the ReactCode
+import React from 'react';
 import { render } from '@testing-library/react';
 
-describe('Hello', () => {
-    it('should render Hello World', () => {
-        const { getByText } = render(<Hello />);
-        expect(getByText('Hello World').textContent).toBe('Hello World');
-    });
+import Hello from './pages/api/hello';
+
+describe('Hello World Component', () => {
+  it('should render correctly', () => {
+    const { getByText } = render(<Hello />);
+    expect(getByText('Hello world!')).toBeInTheDocument();
+  });
 });
