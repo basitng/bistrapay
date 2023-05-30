c1f65ff66f9ff8a6ce5f81d0d5e8e13cf00d4144
Basit Ng <basitng2004@gmail.com>
2023-05-30 14:04:51 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,44 +1,24 @@
-import React from 'react';
- 
-class Users extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users : []
-    };
-  }
-  
-  componentDidMount() {
-    fetch('/api/users')
-        .then(res => res.json())
-        .then(users => this.setState({users}))
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Users</h2>
-        {this.state.users.map(user => 
-          <div key={user.id}>{user.name}</div>
-        )}
-      </div>
-    );
-  }
-}
-
-test('shows a list of users when componentDidMount is called', () => {
-  // Mock the API response
-  fetch.mockResponse(JSON.stringify([{id: 1, name: 'John'}, {id: 2, name: 'Jack'}]));
- 
-  // Render our component
-  const { getByText } = render(<Users />);
- 
-  // Call the componentDidMount lifecycle method
-  act(() => {
-    fireEvent.load(window);
-  });
- 
-  // Assert the users are listed
-  expect(getByText('John')).toBeInTheDocument();
-  expect(getByText('Jack')).toBeInTheDocument();
-});
+const { render, fireEvent } = render(<User />);
+    
+    //Test 1: test email input
+    const emailidentifier =  'input#email';
+    //find the email input element
+    const emailInputElement = container.querySelector(emailidentifier);
+    //simulate a value to be entered 
+    fireEvent.change(emailInputElement, {
+        target: { value: 'test@example.com' },
+    });
+    //check that the value is really entered in the field
+    expect(emailInputElement.value).toBe('test@example.com');
+    
+    
+    //Test 2: test password input
+    const passwordIdentifier =  'input#password';
+    //find the password input element
+    const passwordInputElement = container.querySelector(passwordIdentifier);
+    //simulate a value to be entered 
+    fireEvent.change(passwordInputElement, {
+        target: { value: 'testpassword' },
+    });
+    //check that the value is really entered in the field
+    expect(passwordInputElement.value).toBe('testpassword');
