4c593bea083eab1b7e688522ce9d140005d86017
Basit Ng <basitng2004@gmail.com>
2023-05-30 12:22:37 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,29 +1,15 @@
 import React from 'react';
-     
-     const Manage = () => {
-        const [title, setTitle] = React.useState(""); 
-        const updateTitle = () => {
-            setTitle("Updated Title");
-        }
-
-        return (
-            <div>
-               <button onClick={updateTitle}>Update Title</button>
-               <p>{title}</p>
-            </div>
-        )
-     }
-     
-     // Unit Test
-     import React from 'react';
-     import { render, fireEvent, screen } from '@testing-library/react';
-     
-     test('updates the title correctly when the updateTitle button is clicked', () => {
-        const { container } = render(<Manage />);
-        const updateTitleButton = screen.getByText("Update Title");
-        const titleElement = screen.getByText(/^$/)
-     
-        expect(titleElement.textContent).toBe('');
-        fireEvent.click(updateTitleButton);
-        expect(titleElement.textContent).toBe('Updated Title');
-     });
+    import { render, cleanup } from 'react-testing-library';
+    
+    afterEach(cleanup);
+    
+    const Manage = () => <div><h1>Manage</h1><section>Manage Content</section></div>;
+    
+    describe('Manage', () => {
+      it('should render manage component and content correctly', () => {
+        const { getByTestId, container } = render(<Manage />);
+    
+        expect(getByTestId('manage')).toBeTruthy();
+        expect(container).toMatchSnapshot();
+      });
+    });
