6e5eca49c32dff55116fe22a1410c18447c3d3c4
Basit Ng <basitng2004@gmail.com>
2023-05-30 13:54:30 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,25 +1,16 @@
 import React from 'react';
-    import { render, fireEvent, waitFor } from 'react-testing-library';
-    import Profile from '../../Profile';
+import { render } from '@testing-library/react';
+import Profile from './pages/profile/[profile]';
+
+describe('Profile Component', () => {
+  it(' Should render properly', () => {
+    const { getByText } = render(<Profile />);
+    expect(getByText('Profile Page')).toBeInTheDocument();
+  });
+
+  it('should show profile section when appropriate value is passed', () => {
+    const { getByText } = render(<Profile isProfileView={true} />);
     
-    describe('<Profile />', () => {
-        it('Should render', () => {
-            const { container } = render(<Profile />);
-
-            expect(container.firstChild).toMatchSnapshot(); //to check if the component is rendered
-        })
-    
-        it('Should handle changes', async ()=>{
-            const { getByTestId, getAllByTestId, getByLabelText } = render(<Profile />);
-
-            fireEvent.click(getByTestId('profileCard')); //simulating the button click
-            expect(getAllByTestId('modal').length).toBe(1); //to check if the modal is opened
-
-            fireEvent.change(getByLabelText('Name'), { target: { value: 'John' } }); //simulating the input value change
-            fireEvent.click(getByTestId('modalSaveButton')); //simulating the save button for the modal
-
-            await waitFor(() => {
-                expect(getByLabelText('Name').value).toBe('John'); //to check if the input value is updated
-            });
-        })
-    })
+    expect(getByText('View Profile')).toBeInTheDocument();
+  });
+});
