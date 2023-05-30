0a67b8def50c5aee477994b9d4596c7c5d2e2507
Basit Ng <basitng2004@gmail.com>
2023-05-30 13:53:27 +0100

    Updating unit test for hello.js

--- pages/api/hello.test.js
+++ pages/api/hello.test.js
@@ -1,31 +1,11 @@
-import React from "react";
+import React from 'react'; 
+import { render } from '@testing-library/react'; 
+import App from './App'; 
 
-const HelloPage = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>Welcome to my page.</p>
-    </div>
-  );
-};
-
-export default HelloPage;
-
-// React Testing Library Unit Test for HelloPage Component
-import { render } from "@testing-library/react";
-import React from "react";
-import HelloPage from "pages/api/hello";
-
-describe("HelloPage", () => {
-  test("renders page header", () => {
-    const { getByText } = render(<HelloPage />);
-    const headerElement = getByText("Hello World!");
-    expect(headerElement).toBeInTheDocument();
-  });
-
-  test("renders welcome message", () => {
-    const { getByText } = render(<HelloPage />);
-    const messageElement = getByText("Welcome to my page.");
-    expect(messageElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders the hello world message', () => { 
+    const { getByText } = render(<App />); 
+    const helloWorldMessage = getByText('Hello World'); 
+    expect(helloWorldMessage).toBeInTheDocument(); 
   });
 });
