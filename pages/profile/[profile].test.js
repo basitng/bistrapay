ce5123769d332baac9d5981a5512671360d4d980
Basit Ng <basitng2004@gmail.com>
2023-05-30 14:04:22 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Profile from './pages/profile/[profile]';
+//import React from "react"; 
+import { render, fireEvent } from "@testing-library/react"; 
+import Profile from "../pages/profile/[profile].js";
 
-describe('Profile Component', () => {
-  it(' Should render properly', () => {
-    const { getByText } = render(<Profile />);
-    expect(getByText('Profile Page')).toBeInTheDocument();
-  });
+//Test - can render Profile 
+test('Can render profile', () => {
+    const { getByText } = render(<Profile />); 
+    expect(getByText("My Profile")).toBeInTheDocument();
+});
 
-  it('should show profile section when appropriate value is passed', () => {
-    const { getByText } = render(<Profile isProfileView={true} />);
-    
-    expect(getByText('View Profile')).toBeInTheDocument();
-  });
+//Test - can change name 
+test('Can change name", () => {
+    const { getByLabelText, getByText } = render(<Profile />); 
+    const nameInput = getByLabelText("Name"); 
+    fireEvent.change(nameInput, { target: { value: "Mylene" } }); 
+    expect(getByText("Mylene")).toBeInTheDocument();
+});
+
+//Test - can add to phone list 
+test('Can add to phone list', () => {
+    const { getByLabelText, getByText, getAllByTestId } = render(<Profile />); 
+    const newPhoneInput = getByLabelText("New Phone"); 
+    const phoneNumber = "123-456-7890";
+	fireEvent.change(newPhoneInput, { target: { value: phoneNumber } }); 
+    fireEvent.click(getByText("Add Phone")); 
+    expect(getAllByTestId("phone-list-item")).toHaveLength(2); 
+    expect(getByText(phoneNumber)).toBeInTheDocument();
 });
