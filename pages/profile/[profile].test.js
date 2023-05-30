cd83575facc1ec3573eaffe9c97c812188d37948
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:41:00 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,11 +1,1 @@
-import React from 'react';
-    import { shallow } from 'enzyme';
-    import Profile from './profile';
-    
-    describe('Profile component', () => {
-        it('should render without errors', () => {
-            const component = shallow(<Profile />);
-            const wrapper = component.find('.component');
-            expect(wrapper.length).toBe(1);
-        }); 
-    });
+None
