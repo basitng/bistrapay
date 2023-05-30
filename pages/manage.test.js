9e6a498f4675b12e6bcc5e6a88eeaabc224f4e2e
Basit Ng <basitng2004@gmail.com>
2023-05-30 13:59:11 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,21 +1,59 @@
 import React from 'react';
-    import ManageDashboard from './ManageDashboard';
-     
-    describe('ManageDashboard', () => {
-        it('should render with no errors', () => {             
-            const { container } = render(<ManageDashboard />);             
-            expect(container.firstChild).toMatchSnapshot(); 
-         }); 
- 
-        it('should render a title', () => {
-            const { getByTestId } = render(<ManageDashboard />);
-            const title = getByTestId('title-id');
-            expect(title).toBeInTheDocument();
-         }); 
- 
-        it('should render a table', () => {
-            const { getByTestId } = render(<ManageDashboard />);
-            const table = getByTestId('table-id');
-            expect(table).toBeInTheDocument();
-        });
+
+class ManagePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ''
+        }
+    }
+
+    componentDidMount() {
+        const response = fetch('/api/message')
+            .then(res => res.json())
+            .then(data => this.setState({ message: data.message }));
+    }
+
+    render() {
+        return (
+            <div>
+                Management page:
+                <div>{this.state.message}</div>
+            </div>
+        );
+    }
+}
+
+export default ManagePage;
+
+
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import ManagePage from './manage';
+
+afterEach(cleanup);
+
+//Mock data
+const mockResponse = {
+    message: 'Manage page!'
+}
+
+//Mock API fetch call
+global.fetch = jest.fn().mockImplementation(() => {
+    return Promise.resolve({
+        json: () => Promise.resolve(mockResponse)
     });
+});
+
+describe('ManagePage Component', () => {
+    test('renders manage page message', async () => {
+        //Arrange
+        const { findByTestId } = render(<ManagePage />);
+
+        //Act
+        const manageElement = await findByTestId('message');
+
+        //Assert
+        expect(manageElement.innerHTML).toBe('Manage page!');
+    });
+});
