86577250d28bf9f9b99842525cce42ac19a29aed
Basit Ng <basitng2004@gmail.com>
2023-05-30 23:14:39 +0100

    Updating unit test for user.reducer.js

--- src/context/user.reducer.test.js
+++ src/context/user.reducer.test.js
@@ -1,58 +1,33 @@
 import React from 'react';
-        import { shallow } from 'enzyme';
-        import { userReducer } from './user.reducer';
-        
-        describe('userReducer', () => {
-            test('should set an initial state', () => {
-            const expectedState = {
-                username: null,
-                role: null
-            };
-            expect(userReducer(undefined, {})).toEqual(expectedState);
-            });
+    import {userReducer, initialState} from './user.reducer';
 
-            test('should return the current state if no valid actions have been made', () => {
-                // Section for writing the unit test
-            });
+    describe('User Reducer', () => {
+        it('should return initial state when no action is passed', () => {
+            expect(userReducer(undefined, {})).toEqual(initialState);
+        });
 
-            test('should set username', () => {
-                // Section for writing the unit test
-            });
-
-            test('should set role', () => {
-                // Section for writing the unit test
+        it('should handle GET_USER action correctly', () => {
+            const getUserAction = {
+                type: 'GET_USER',
+                payload: {
+                    id: '123',
+                    username: 'JohnDoe'
+                }
+            }
+            expect(userReducer(undefined, getUserAction)).toEqual({
+                user: {
+                    id: '123',
+                    username: 'JohnDoe'
+                }
             });
         });
 
-// Test for returning the current state if no valid  actions have been made
-const state = {
-    username: "john",
-    role: "admin"
-};
-
-expect(userReducer(state, { type: null })).toEqual(state);
-
-// Test for setting username
-const action = {
-    type: 'SET_USERNAME',
-    username: 'mark'
-};
-const expectedState = {
-    username: 'mark',
-    role: null
-};
-
-expect(userReducer(state, action)).toEqual(expectedState);
-
-// Test for setting role
-const action2 = {
-    type: 'SET_ROLE',
-    role: 'admin'
-};
-
-const expectedState2 = {
-    username: "john",
-    role: "admin"
-};
-
-expect(userReducer(state, action2)).toEqual(expectedState2);
+        it('should handle LOGOUT_USER action correctly', () => {
+            const logoutUserAction = {
+                type: 'LOGOUT_USER'
+            }
+            expect(userReducer(undefined, logoutUserAction)).toEqual({
+                user: {}
+            });
+        });
+    });
