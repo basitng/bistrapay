f16c35068811baf75062ff21314f878363c83057
Basit Ng <basitng2004@gmail.com>
2023-05-30 18:12:17 +0100

    Updating unit test for hello.js

--- pages/api/hello.test.js
+++ pages/api/hello.test.js
@@ -1,39 +1,45 @@
 import React from 'react';
-    import {render, fireEvent } from '@testing-library/react';
-    import Hello from './hello';
 
-    describe('Hello', () => {
-        it('should display a greeting', () => {
-            //Arrange
-            const name = 'Dan';
-            const title = 'Developer';
+     export const Hello = () => {
+       const [message, setMessage] = React.useState('Welcome to React');
 
-            const { getByText } = render(<Hello name={name} title={title} />)
-             
-            // Act
-            const element = getByText(/hello/i);
+       return (
+         <div>
+           <div className="message">{message}</div>
+           <button onClick={() => setMessage('Hello From React!')}>
+             Click Me
+           </button>
+         </div>
+       );
+     };
 
-            // Assert
-            expect(element).toBeInTheDocument();
-            expect(element).toHaveTextContent('Hello Dan! You are a Developer');
+
+      import React from 'react';
+      import { render, fireEvent, screen } from '@testing-library/react';
+      import { Hello } from './Hello';
+
+      //Author: John Doe
+      //Date: mm/dd/yyyy
+
+      describe('Hello', () => {
+        //Arrange
+        beforeEach(() => {
+          render(<Hello />);
         });
 
-        it('the text should change when a new name is provided', () => {
-            //Arrange
-            const name = 'Test';
-            const title = 'Tester';
-
-            const { getByText, getByLabelText } = render(<Hello name={name} title={title} />)
-
-            const nameInput = getByLabelText(/name/i);
-            const titleInput = getByLabelText(/title/i);
-
-            // Act
-            fireEvent.change(nameInput, { target: { value: 'Test' } });
-            fireEvent.change(titleInput, { target: { value: 'Tester' } });
-
-            // Assert
-            const element = getByText('Hello Test! You are a Tester');
-            expect(element).toBeInTheDocument();
+        it('are the correct initial message is displayed', () => {
+           //Act
+           const messageElement = screen.getByText(/Welcome to React/i);
+          
+           //Assert
+           expect(messageElement).toBeInTheDocument();
         });
-    });
+
+        it('are the updated message is displayed after clicking the button', () => {
+           //Act
+           fireEvent.click(screen.getByText(/Click Me/i));
+
+           //Assert
+          expect(screen.getByText(/Hello From React/i)).toBeInTheDocument();
+        });
+      });
