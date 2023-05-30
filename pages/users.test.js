8983aa94cf30d78be4f80afa3b3c11cb43352ebf
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:39:48 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,18 +1,1 @@
-import React from 'react'
-        import { render, cleanup } from 'react-testing-library'
-        
-        afterEach(cleanup)
-        
-        describe('rendering users component', () => {
-            it('displays correct header', () => {
-                const { getByText } = render(<h1>User</h1>)
-                
-                expect(getByText('User').textContent).toBe('User')
-            })
-        
-            it('displays the correct user count', () => {
-                const { getByText } = render(<h3>Total Users: 5</h3>)
-                
-                expect(getByText('Total Users: 5').textContent).toBe('Total Users: 5')
-            })
-        })
+None
