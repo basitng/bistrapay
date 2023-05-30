bf5ea717ef5c06743f3000172b126c0baa9c4391
Basit Ng <basitng2004@gmail.com>
2023-05-30 18:14:29 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,28 +1,29 @@
-//import React from "react"; 
-import { render, fireEvent } from "@testing-library/react"; 
-import Profile from "../pages/profile/[profile].js";
+import React from 'react';
+    import { render } from '@testing-library/react';
+    import Profile from './Profile';
 
-//Test - can render Profile 
-test('Can render profile', () => {
-    const { getByText } = render(<Profile />); 
-    expect(getByText("My Profile")).toBeInTheDocument();
-});
+    describe('Profile', () => {
+        it('should render properly', () => {
+            const { container } = render(<Profile />);
+            expect(container).toMatchSnapshot();
+        });
 
-//Test - can change name 
-test('Can change name", () => {
-    const { getByLabelText, getByText } = render(<Profile />); 
-    const nameInput = getByLabelText("Name"); 
-    fireEvent.change(nameInput, { target: { value: "Mylene" } }); 
-    expect(getByText("Mylene")).toBeInTheDocument();
-});
+        it('should render a form to update the profile info', () => {
+            const { getByText, getByLabelText } = render(<Profile />);
+            expect(getByText('Update Profile')).toBeInTheDocument();
+            expect(getByLabelText('Username')).toBeInTheDocument();
+            expect(getByLabelText('Email Address')).toBeInTheDocument();
+            expect(getByLabelText('Display Name')).toBeInTheDocument();
+        });
 
-//Test - can add to phone list 
-test('Can add to phone list', () => {
-    const { getByLabelText, getByText, getAllByTestId } = render(<Profile />); 
-    const newPhoneInput = getByLabelText("New Phone"); 
-    const phoneNumber = "123-456-7890";
-	fireEvent.change(newPhoneInput, { target: { value: phoneNumber } }); 
-    fireEvent.click(getByText("Add Phone")); 
-    expect(getAllByTestId("phone-list-item")).toHaveLength(2); 
-    expect(getByText(phoneNumber)).toBeInTheDocument();
-});
+        it('should update the profile once the submit button is clicked', () => {
+            const { getByText, getByLabelText } = render(<Profile />);
+
+            getByLabelText('Username').value = 'username';
+            getByLabelText('Email Address').value = 'test@example.com';
+            getByLabelText('Display Name').value = 'displayName';
+            getByText('Update Profile').click();
+
+            expect(getByText('User profile updated successfully!')).toBeInTheDocument();
+        });
+    });
