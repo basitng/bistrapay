fceaaf3a6cfff0b8130c2c06153c8422599912f1
Basit Ng <basitng2004@gmail.com>
2023-05-30 23:02:47 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,1 +1,20 @@
-None
+import React from 'react';
+        
+        import {shallow} from 'enzyme';
+        
+        import Manage from './Manage';
+        
+        describe('Manage component', () => {
+            test('renders without crashing', () => {
+                const wrapper = shallow(<Manage />);
+                expect(wrapper).toBeDefined();
+            });
+            
+            test('calls the handleSubmit method when form is submitted', () => {
+                const wrapper = shallow(<Manage />);
+                const handleSubmitSpy = jest.spyOn(wrapper.instance(), 'handleSubmit');
+                wrapper.update();
+                wrapper.find('form').simulate('submit');
+                expect(handleSubmitSpy).toHaveBeenCalled();
+            });
+        });
