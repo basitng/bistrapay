6636f561f9551c4633c9d675f0ca70b31f2f638d
Basit Ng <basitng2004@gmail.com>
2023-05-30 13:35:47 +0100

    Updating unit test for index.js

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,22 +1,29 @@
-import React from "react";
-    import { render } from "react-testing-library";
-    import MyComponent from "./MyComponent";
+import React from 'react';
     
-    describe("MyComponent", () => {
-      // Write your test cases here
-        test('MyComponent should render correct content', () => {
-           const { getByText } = render(<MyComponent />);
-           expect(getByText('MyComponent Text')).toBeInTheDocument();
-        });
-        
-        test('MyComponent should render a button', () => {
-           const { getByText } = render(<MyComponent />);
-           expect(getByText('My Button')).toBeInTheDocument();
-        });
-        
-        test('Button should be disabled on initial load', () => {
-           const { getByText } = render(<MyComponent />);
-           const button = getByText('My Button');
-           expect(button).toBeDisabled();
-        });
+    const IndexPage = () => {
+        return (
+            <div>
+                <h1>Welcome to my homepage!</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur purus sed erat elementum, vel lacinia urna volutpat. Nunc varius ligula eu magna tincidunt, in volutpat nisi rutrum.
+                </p>
+            </div>
+        );
+    };
+    
+    export default IndexPage;
+
+import React from 'react';
+import { render } from '@testing-library/react';
+
+describe('Index page', () => {
+    it('should display a welcome message', () => {
+        const { getByText } = render(<IndexPage />);
+        expect(getByText('Welcome to my homepage!')).toBeInTheDocument();
     });
+
+    it('should display a message with lorem ipsum text', () => {
+        const { getByText } = render(<IndexPage />);
+        expect(getByText(/Lorem ipsum/)).toBeInTheDocument();
+    });
+});
