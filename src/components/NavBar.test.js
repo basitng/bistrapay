4cf6b451ae50d4bd5367b55746861e58ea53673e
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:43:33 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,1 +1,28 @@
-None
+describe('NavBar', () => {
+  let wrapper;
+  
+  beforeEach(() => {
+    wrapper = shallow(<NavBar />);
+  });
+
+  it('should render without error', () => {
+    const navbarComponent = wrapper.find(".nav-container");
+    expect(navbarComponent.length).toBe(1);
+  });
+  
+  it('should render a logo', () => {
+    const logoImg = wrapper.find(".logo");
+    expect(logoImg.length).toBe(1);
+  });
+  
+  it('should render a searchbar', () => {
+    const searchbar = wrapper.find('.searchbar');
+    expect(searchbar.length).toBe(1);
+  });
+  
+  it('should render menu items', () => {
+    const menuItemList = wrapper.find('.menu-item');
+    expect(menuItemList.length).toBeGreaterThan(0);
+  });
+  
+  });
