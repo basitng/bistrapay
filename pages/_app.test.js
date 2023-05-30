6887eadf2355b47237a7a3af93fff4f4423ced6f
Basit Ng <basitng2004@gmail.com>
2023-05-30 13:41:23 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,11 +1,12 @@
-import React from 'react';
-    import {render} from '@testing-library/react';
-
-    const App = () => {
-        return <div>Hello App</div>
-    }
+import React from "react";
     
-    test('Renders App component', () => {
-        const {container} = render(<App />);
-        expect(container).not.toBeNull();
-    })
+    import { render } from "@testing-library/react";
+    import App from "./_app";
+
+    describe("/pages/_app.js", () => {
+	  it("should render the App component", () => {
+	    const { getByText } = render(<App />);
+	    const header = getByText("Welcome");
+	    expect(header).toBeInTheDocument();
+	  });
+	});
