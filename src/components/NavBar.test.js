23b9fd04ccf9fdf02fc2065d8f98096169f7a1a2
Basit Ng <basitng2004@gmail.com>
2023-05-30 13:55:38 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,31 +1,27 @@
 import React from 'react';
-    import {render, fireEvent, cleanup } from '@testing-library/react'
-    import NavBar from './NavBar';
-    
-    // Test 
-    describe("NavBar component test", () => {
-        afterEach(cleanup);
+import Navbar from './Navbar';
+import {render, fireEvent, screen} from '@testing-library/react';
 
-        it ("Renders nav without crashing", () => {
-            const { getByTestId } =  render(<NavBar />)
-            const navBar = getByTestId('NavBar');
-            expect(navBar).not.toBeNull();
-        });
-    
-        it("Should display a NavBar with lists of items when clicked on hamburger icon",  () => {
-            const { getByTestId, getByText } =  render(<NavBar />);
-            const hamburgerIcon = getByTestId('hamburgerIcon');
-            fireEvent.click(hamburgerIcon);
-            const listItems = getByTestId('nav-list');
-            expect(listItems).not.toBeNull();
-        });
-
-        it("Should call a onClick function", () => {
-            const onClickFn = jest.fn();
-            const { getByTestId } =  render(<NavBar onClick={onClickFn} />);
-            const hamburgerIcon = getByTestId('hamburgerIcon');
-            fireEvent.click(hamburgerIcon);
-            expect(onClickFn).toHaveBeenCalled(); 
-        });
-
+describe('Navbar component', () => {
+    it('should render without errors', () => {  
+        render(<Navbar />);
+        const navbar = screen.getByTestId('navbarComponent');
+        expect(navbar).toBeInTheDocument();
     });
+
+    it('should navigate to the about page when the about link is clicked', () => {
+        render(<Navbar />);
+        const aboutLink = screen.getByTestId('about-link');
+        fireEvent.click(aboutLink);
+        expect(window.location.pathname).toBe("/about");
+    })
+
+
+    it('should navigate to the contact page when the contact link is clicked', () => {
+        render(<Navbar />);
+        const contactLink = screen.getByTestId('contact-link');
+        fireEvent.click(contactLink);
+        expect(window.location.pathname).toBe("/contact");
+    });
+
+});
