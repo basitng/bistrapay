4b4b1fc2625c9a4ca459d878a768938ad6b3a32b
Basit Ng <basitng2004@gmail.com>
2023-05-30 12:23:15 +0100

    Updating unit test for [profile].js

--- pages/profile/[profile].test.js
+++ pages/profile/[profile].test.js
@@ -1,23 +1,25 @@
 import React from 'react';
+    import { render, fireEvent, waitFor } from 'react-testing-library';
+    import Profile from '../../Profile';
     
-    import Profile from './index';
-    
-    import { render, cleanup } from 'react-testing-library';
-    
-    afterEach(cleanup);
-    
-    describe('<Profile />', ()=>{
-        it('should render without errors', ()=>{
-            render(<Profile />);
+    describe('<Profile />', () => {
+        it('Should render', () => {
+            const { container } = render(<Profile />);
+
+            expect(container.firstChild).toMatchSnapshot(); //to check if the component is rendered
         })
     
-        it('should contain the correct name and profile picture', ()=>{
-            const { getByText, getByTestId } = render(<Profile name="name" imgUrl="picture.png"/>);
-    
-            const nameNode = getByText('name');
-            expect(nameNode).toBeTruthy();
-    
-            const profilePictureNode = getByTestId('profile-picture')
-            expect(profilePictureNode).toHaveAttribute('src', 'picture.png');
+        it('Should handle changes', async ()=>{
+            const { getByTestId, getAllByTestId, getByLabelText } = render(<Profile />);
+
+            fireEvent.click(getByTestId('profileCard')); //simulating the button click
+            expect(getAllByTestId('modal').length).toBe(1); //to check if the modal is opened
+
+            fireEvent.change(getByLabelText('Name'), { target: { value: 'John' } }); //simulating the input value change
+            fireEvent.click(getByTestId('modalSaveButton')); //simulating the save button for the modal
+
+            await waitFor(() => {
+                expect(getByLabelText('Name').value).toBe('John'); //to check if the input value is updated
+            });
         })
     })
