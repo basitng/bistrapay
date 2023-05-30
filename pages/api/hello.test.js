859ca4b30bf29688424af1f2a0ecf654e370cd28
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:57:18 +0100

    Updating unit test for hello.js

--- pages/api/hello.test.js
+++ pages/api/hello.test.js
@@ -1,1 +1,41 @@
-None
+import React from "react";
+    import UserProfile from "../../components/UserProfile";
+    import { render, unmountComponentAtNode } from "react-dom";
+    import { act } from "react-dom/test-utils";
+
+    let container = null;
+    beforeEach(() => {
+      // setup a DOM element as a render target
+      container = document.createElement("div");
+      document.body.appendChild(container);
+    });
+    
+    afterEach(() => {
+      // cleanup on exiting
+      unmountComponentAtNode(container);
+      container.remove();
+      container = null;
+    });
+    
+    describe("Test UserProfile component", () => {
+
+      test("Renders the component & check the user data provided", () => {
+        const userData = {
+          name: 'John',
+          surname: 'Doe',
+          imgPath: './images/avatar.jpg'
+        };
+        
+        act(() => {
+          render(<UserProfile userData={userData}/>, container);
+        });
+        
+        const userNameElem = container.querySelector('#name');
+        const surnameElem = container.querySelector('#surname');
+        const avatarElem = container.querySelector('#avatar');
+
+        expect(userNameElem.textContent).toBe(userData.name);
+        expect(surnameElem.textContent).toBe(userData.surname);
+        expect(avatarElem.src).toContain(userData.imgPath);
+    });
+});
