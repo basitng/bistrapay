038f8a0c84e5ac81e77bd30d4178bc5b1ae7cb18
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:39:54 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,14 +1,1 @@
-import React from "react";
-    import { render, cleanup } from "react-testing-library";
-    import NavBar from "./NavBar";
-    
-    afterEach(cleanup);
-    
-    describe("NavBar", () => {
-        it('renders the correct number of NavBar components', () => {
-            const { getAllByTestId } = render(
-                <NavBar items={['Home','About', 'Contact']} />
-            );
-            expect(getAllByTestId('nav-bar').length).toBe(3);
-        });
-    });
+None
