463c5d4c06d0e37514cd6b90057c85d1935044f4
Basit Ng <basitng2004@gmail.com>
2023-05-30 12:24:15 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,21 +1,26 @@
-//Solution
-        //using react-testing-library
+import React from 'react';
+    import {UsersList} from './UsersList';
+
+    describe('UsersList component', () => {
+        let wrapper;
+        beforeEach(() => {
+            wrapper = shallow(<UsersList />)
+        }); 
         
-        import React from 'react';
-        import { render, screen } from '@testing-library/react';
-        import Users from '../pages/users';
-
-        test('User should be properly displayed', () => {
-            const users = [
-                {name:'John Smith', email: 'JohnSmith@example.com'},
-                {name:'Kevin Jackson', email: 'KevinJackson@example.com'}
-            ];
-            render(<Users users={users} />);
-
-            users.forEach(user => {
-                const name = screen.getByText(user.name);
-                const email = screen.getByText(user.email);
-                expect(name).toBeInTheDocument();
-                expect(email).toBeInTheDocument();
-            });
+        test('should render the title', () => {
+            const title = wrapper.find('h2');
+            expect(title.text()).toBe('List of All Users:');
+        }); 
+        
+        test('should render a list of users', () => {
+            expect(wrapper.find('ul').exists).toBe(true);
+            expect(wrapper.find('li').length).toBe(10);
         });
+        
+        test('it should contain a name for each user', () => {
+            // check every li item
+            wrapper.find('li').forEach(node => {
+                expect(node.text.includes('Name:')).toBeTruthy();
+            });
+        }); 
+    });
