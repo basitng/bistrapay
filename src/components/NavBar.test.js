b1053229792ba3ef503fdc7d7cc9219877e117a2
Basit Ng <basitng2004@gmail.com>
2023-05-30 23:14:07 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,19 +1,9 @@
-//Use the React Testing Library
-    import React from 'react';
-    import {render, fireEvent, screen} from "@testing-library/react"
+import React from 'react';
+    import { shallow } from 'enzyme';
     import NavBar from './NavBar';
-    
-    describe('NavBar Component', () => {
-        it('should render the NavBar correctly', () => {
-            const {getByRole} = render(<NavBar />);
-            expect(getByRole("navigation").children.length).toBe(3)
-        });
 
-        it("should call the onChange when the input changes", () => {
-            const mockHandleChange = jest.fn()
-            const {getByRole} = render(<NavBar onChange={mockHandleChange}/>)
-            const input = getByRole("textbox")
-            fireEvent.change(input, {target: {value: "This is a test"}})
-            expect(mockHandleChange).toHaveBeenCalledTimes(1)
-        })
-    })
+    describe('NavBar component', () => {
+        it('renders without crashing', () => {
+            shallow(<NavBar />);
+        });
+    });
