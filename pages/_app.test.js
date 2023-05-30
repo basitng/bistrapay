e0d61d2a6f499967e65e982e63109d969712c718
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:42:16 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,1 +1,10 @@
-None
+//Using Jest Library
+    describe('App Component', () => {
+        it('renders correctly', () => {
+            const component = shallow(
+                <App />
+            );
+
+            expect(component).toMatchSnapshot();
+        });
+    })
