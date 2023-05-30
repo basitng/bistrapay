7f77fdec80bd242664e2a5a1c5af9f2fe26a9452
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:43:02 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,1 +1,10 @@
-None
+import React from 'react';
+    import { shallow } from 'enzyme';
+    import Users from './users';
+
+    describe('Users Component', () => {
+      test('should render correctly', () => {
+        const wrapper = shallow(<Users />);
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
