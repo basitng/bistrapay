744da50c8ea34f59853de508919a934274da851f
Basit Ng <basitng2004@gmail.com>
2023-05-30 20:47:22 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import NavBar from './NavBar';
-import { render, fireEvent } from "@testing-library/react";
-
-test('Clicking link should change page', () => { 
-    // Initial Render of the component
-    const { getByText } = render(<NavBar />);
-
-    // Assert link is present
-    const linkElement = getByText(/Home/i);
-    expect(linkElement).toBeInTheDocument();
-
-    // Fire Event
-    fireEvent.click(linkElement);
-
-    // Assert new page and link to be presented
-    const aboutLinkElement = getByText(/About/i);
-    expect(aboutLinkElement).toBeInTheDocument();
-});
+import React from "react";
+    import { render, cleanup } from "react-testing-library";
+    import NavBar from "./NavBar";
+    
+    afterEach(cleanup);
+    
+    describe("NavBar", () => {
+        it('renders the correct number of NavBar components', () => {
+            const { getAllByTestId } = render(
+                <NavBar items={['Home','About', 'Contact']} />
+            );
+            expect(getAllByTestId('nav-bar').length).toBe(3);
+        });
+    });
