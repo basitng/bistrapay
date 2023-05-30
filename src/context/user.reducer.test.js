68017661386bbf9668f6cd6312e73589baef8186
Basit Ng <basitng2004@gmail.com>
2023-05-30 12:26:39 +0100

    Updating unit test for user.reducer.js

--- src/context/user.reducer.test.js
+++ src/context/user.reducer.test.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import { mount, shallow } from 'enzyme'
-import { userReducer } from './user.reducer'
+// Unit test written with react-testing-library
+import { render, fireEvent,  waitForElement } from '@testing-library/react'
+import userReducer from '../src/context/user.reducer';
 
-describe('userReducer', () => { 
-  let store;
-
-  beforeEach(() => {
-    store = createStore(userReducer)
-  })
-
-  test('Update the user name', () => {
-    expect(userReducer({name:'mark'}, {type:'UPDATE_USER_NAME', name:'mark jones'})).toEqual({name:'mark jones'})
-  }) 
-
-  test('Delete user name', () => {
-    expect(userReducer({name:'mark'}, {type:'DELETE_USER_NAME'})).toEqual({name:''})
-  }) 
-
+describe('Testing user.reducer', () => {
+    
+    let initialState 
+    beforeEach(() => {
+      initialState = {
+        data: {},
+        isLoading: false,
+        hasError: false
+      }
+    })
+    
+    // Test for FETCH_STARTED
+    test('should dispatch FETCH_STARTED', () => {
+        const newState = userReducer(initialState, {type: 'FETCH_STARTED'})
+        expect(newState.isLoading).toBe(true)
+        expect(newState.hasError).toBe(false)
+    })
+    
+    // Test for FETCH_SUCCESS
+    test('should dispatch FETCH_SUCCESS', () => {
+        const data = {test_data: 123}
+        const newState = userReducer(initialState, {type: 'FETCH_SUCCESS', payload: data})
+        expect(newState.data).toBe(data)
+        expect(newState.isLoading).toBe(false)
+    })
+    
+    // Test for FETCH_ERROR
+    test('should dispatch FETCH_ERROR', () => {
+        const newState = userReducer(initialState, {type: 'FETCH_ERROR'})
+        expect(newState.isLoading).toBe(false)
+        expect(newState.hasError).toBe(true)
+    })
 })
