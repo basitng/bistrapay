3b45b0209747a2688aadd98de14ad9e707288eea
Basit Ng <basitng2004@gmail.com>
2023-05-30 14:05:11 +0100

    Updating unit test for NavBar.js

--- src/components/NavBar.test.js
+++ src/components/NavBar.test.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import Navbar from './Navbar';
-import {render, fireEvent, screen} from '@testing-library/react';
+import { render } from '@testing-library/react';
+import NavBar from './NavBar'
 
-describe('Navbar component', () => {
-    it('should render without errors', () => {  
-        render(<Navbar />);
-        const navbar = screen.getByTestId('navbarComponent');
-        expect(navbar).toBeInTheDocument();
-    });
-
-    it('should navigate to the about page when the about link is clicked', () => {
-        render(<Navbar />);
-        const aboutLink = screen.getByTestId('about-link');
-        fireEvent.click(aboutLink);
-        expect(window.location.pathname).toBe("/about");
+describe("NavBar Component", () => {
+    test('Component is rendered correctly in UI', ()=>{
+        const { getByText } = render(<NavBar />);
+        expect(getByText("Home")).toBeInTheDocument();
+        expect(getByText("About")).toBeInTheDocument();
+        expect(getByText("FAQ")).toBeInTheDocument();
     })
-
-
-    it('should navigate to the contact page when the contact link is clicked', () => {
-        render(<Navbar />);
-        const contactLink = screen.getByTestId('contact-link');
-        fireEvent.click(contactLink);
-        expect(window.location.pathname).toBe("/contact");
-    });
-
-});
+})
