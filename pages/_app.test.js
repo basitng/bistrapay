01d8d7fb139b69960336493657b872f14a3c30e2
Basit Ng <basitng2004@gmail.com>
2023-05-30 18:11:42 +0100

    Updating unit test for _app.js

--- pages/_app.test.js
+++ pages/_app.test.js
@@ -1,10 +1,19 @@
-import React from "react";
-    import { render } from "@testing-library/react";
-    import App from "./_app";
+import React from 'react';
+    import { render } from '@testing-library/react';
     
-    describe('_app', () => {
-      it('should render successfully', async () => {
-        const { baseElement } = render(<App />);
-        expect(baseElement).toBeInTheDocument();
-      });
+    const App = () => {
+    return (
+        <div>
+        <h1>Welcome</h1>
+        <p>This is App component.</p>
+        </div>
+    );
+    }
+    
+    describe("App component", () => {
+    it("should render a heading and a paragarph", () => {
+        const { getByText } = render(<App />);
+        expect(getByText("Welcome")).toBeInTheDocument();
+        expect(getByText("This is App component.")).toBeInTheDocument();
+    });
     });
