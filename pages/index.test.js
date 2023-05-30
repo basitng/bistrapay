0a69731a9f4b9cddf7e9c4392fea48f4943383e7
Basit Ng <basitng2004@gmail.com>
2023-05-30 22:40:44 +0100

    Updating unit test for index.js

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,1 +1,8 @@
-None
+//using jest to write the unit test
+    describe('Index page', () => {
+        test('Determine if the page is rendered correctly', () => {
+            //generate a mock of the component
+            const renderedComponent = shallow(<Index />);
+            expect(renderedComponent).toMatchSnapshot();
+        });
+    });
