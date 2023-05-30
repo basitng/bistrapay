35f6df53f7ea94be4203ff0577349e1bc9384a31
Basit Ng <basitng2004@gmail.com>
2023-05-30 23:03:25 +0100

    Updating unit test for users.js

--- pages/users.test.js
+++ pages/users.test.js
@@ -1,1 +1,25 @@
-None
+One possible solution:
+        import { expect } from 'chai';
+        import React from 'react';
+        import { shallow } from 'enzyme';
+        
+        import Users from './pages/Users';
+        
+        describe('Users component', () => {
+            let wrapper;
+            beforeEach(() => {
+                wrapper = shallow(<Users />);
+            });
+            
+            it('should render successfully', () => {
+                expect(wrapper).to.have.lengthOf(1);
+            });
+            
+            it('should render the component `div` element', () => {
+                expect(wrapper.find('div')).to.have.lengthOf(1);
+            });
+            
+            it('should contain a title', () => {
+               expect(wrapper.find('h1').text()).to.equal('Users');
+            });
+        });
