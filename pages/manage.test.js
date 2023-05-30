6b96677bbb560c1943659c2929cd355f5ca45656
Basit Ng <basitng2004@gmail.com>
2023-05-30 13:54:13 +0100

    Updating unit test for manage.js

--- pages/manage.test.js
+++ pages/manage.test.js
@@ -1,58 +1,21 @@
 import React from 'react';
-
-class ManagePage extends React.Component {
-
-  state = {
-    text: '',
-    isActive: false
-  };
-
-  handleTextChange = (e) => {
-    this.setState({ text: e.target.value });
-  }
-
-  handleSubmitForm = () => {
-    if (this.state.text.trim().length > 4) {
-      this.setState({ isActive: true });
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="form">
-          <input
-            type="text"
-            value={this.state.text}
-            onChange={this.handleTextChange}
-          />
-        </div>
-        <button onClick={this.handleSubmitForm} disabled={!this.state.isActive}>
-          Submit
-        </button>
-      </div>
-    );
-  }
-}
-
-export default ManagePage;
-
-
-import React from 'react';
-import { render, fireEvent, act } from '@testing-library/react';
-
-describe('ManagePage', () => {
-    it('should enable the submit button when enough characters have been entered', () => {
-        const { getByText, container, getByPlaceholderText } = render(<ManagePage />);
-        const input = getByPlaceholderText("Enter text...");
-        const submitButton = getByText('Submit');
-
-        expect(submitButton.disabled).toEqual(true);
-
-        act(() => {
-            fireEvent.change(input, { target: { value: 'test' } });
+    import ManageDashboard from './ManageDashboard';
+     
+    describe('ManageDashboard', () => {
+        it('should render with no errors', () => {             
+            const { container } = render(<ManageDashboard />);             
+            expect(container.firstChild).toMatchSnapshot(); 
+         }); 
+ 
+        it('should render a title', () => {
+            const { getByTestId } = render(<ManageDashboard />);
+            const title = getByTestId('title-id');
+            expect(title).toBeInTheDocument();
+         }); 
+ 
+        it('should render a table', () => {
+            const { getByTestId } = render(<ManageDashboard />);
+            const table = getByTestId('table-id');
+            expect(table).toBeInTheDocument();
         });
-        expect(input.value).toEqual('test');
-        expect(submitButton.disabled).toEqual(false);
     });
-});
